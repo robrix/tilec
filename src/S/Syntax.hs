d28fefbf59762e4db16b0a30d12c65ea55bf3a72
Rob Rix <rob.rix@me.com>
2020-02-22 09:50:59 -0500

    Derive Foldable, Functor, & Traversable instances.

--- src/S/Syntax.hs
+++ src/S/Syntax.hs
@@ -1,3 +1,4 @@
+{-# LANGUAGE DeriveTraversable #-}
 module S.Syntax
 ( Term(..)
 , Spine(..)
@@ -6,11 +7,13 @@ module S.Syntax
 data Term a
   = Abs (Term (Maybe a))
   | a :$ Spine (Term a)
+  deriving (Foldable, Functor, Traversable)
 
 infixl 9 :$
 
 data Spine a
   = Nil
   | Spine a :> a
+  deriving (Foldable, Functor, Traversable)
 
 infixl 5 :>
