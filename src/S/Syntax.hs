a214925b132cee6f435fad6c988eb62c607260ea
Rob Rix <rob.rix@me.com>
2020-02-22 10:01:02 -0500

    Define a type of problems.

--- src/S/Syntax.hs
+++ src/S/Syntax.hs
@@ -1,11 +1,17 @@
 {-# LANGUAGE DeriveTraversable #-}
 module S.Syntax
 ( Term(..)
+, Prob(..)
 , Expr(..)
 , Spine(..)
 ) where
 
 newtype Term a = Term (Expr Term a)
+  deriving (Foldable, Functor, Traversable)
+
+data Prob a
+  = Ex (Prob (Maybe a))
+  | Prob (Expr Prob a)
   deriving (Foldable, Functor, Traversable)
 
 data Expr t a
