0211b6c9c841c307db7e4d964f7afb920684c9b6
Rob Rix <rob.rix@me.com>
2020-02-23 08:50:55 -0500

    Stub in a datatype for problem terms.

--- src/S/Problem.hs
+++ src/S/Problem.hs
@@ -1,2 +1,14 @@
 module S.Problem
-() where
+( Term(..)
+) where
+
+import S.Scope
+
+data Term a
+  = Var a
+  | Abs (Scope Term a)
+  | Term a :$ Term a
+  | Type
+  | Pi (Term a) (Scope Term a)
+
+infixl 9 :$
