7236d9c677509ef84ed7e95e93a931b40811e05e
Rob Rix <rob.rix@me.com>
2020-02-23 11:05:59 -0500

    Check lambdas.

--- src/S/Elab.hs
+++ src/S/Elab.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE GADTs #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE TypeOperators #-}
 module S.Elab
 ( check
@@ -6,13 +7,19 @@ module S.Elab
 , Ctx(..)
 ) where
 
+import           Bound.Scope
 import           Bound.Var
 import qualified S.Core as Core
 import qualified S.Problem as Problem
 import           S.Syntax
 
-check :: MonadFail m => Problem.Term a -> Core.Term a -> m (Core.Term a ::: Core.Term a)
-check _ _ = fail "unimplemented"
+check :: MonadFail m => Ctx a -> Problem.Term a ::: Core.Term a -> m (Core.Term a ::: Core.Term a)
+check ctx = \case
+  Problem.Abs b ::: Core.Pi ta tb -> do
+    b' ::: tb' <- check (ctx :- ta) (fromScope b ::: fromScope tb)
+    pure (Core.Abs (toScope b') ::: Core.Pi ta (toScope tb'))
+
+  _ -> fail "unimplemented"
 
 infer :: MonadFail m => Problem.Term a -> m (Core.Term a ::: Core.Term a)
 infer _ = fail "unimplemented"
