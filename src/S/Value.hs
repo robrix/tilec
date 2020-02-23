9a30f76d00599dac7f6de93a1d9998f6c70eded9
Rob Rix <rob.rix@me.com>
2020-02-23 16:55:06 -0500

    Define a datatype of Values.

--- src/S/Value.hs
+++ src/S/Value.hs
@@ -1,2 +1,11 @@
 module S.Value
-() where
+( Value(..)
+) where
+
+import S.Syntax
+
+data Value a
+  = Lam (Value a -> Value a)
+  | a :$ Spine (Value a)
+  | Type
+  | Pi (Value a) (Value a -> Value a)
