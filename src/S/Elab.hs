7198522ff6d49023eb6656434bc4620d990e6687
Rob Rix <rob.rix@me.com>
2020-02-23 11:03:03 -0500

    Define a datatype of contexts.

--- src/S/Elab.hs
+++ src/S/Elab.hs
@@ -1,9 +1,12 @@
+{-# LANGUAGE GADTs #-}
 {-# LANGUAGE TypeOperators #-}
 module S.Elab
 ( check
 , infer
+, Ctx(..)
 ) where
 
+import           Bound.Var
 import qualified S.Core as Core
 import qualified S.Problem as Problem
 import           S.Syntax
@@ -13,3 +16,10 @@ check _ _ = fail "unimplemented"
 
 infer :: MonadFail m => Problem.Term a -> m (Core.Term a ::: Core.Term a)
 infer _ = fail "unimplemented"
+
+
+data Ctx a where
+  CNil :: Ctx a
+  (:-) :: Ctx a -> Core.Term a -> Ctx (Var () a)
+
+infixl 5 :-
