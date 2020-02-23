5d07e981e3b28ea49c1b8393ceb3bd58e733e9d5
Rob Rix <rob.rix@me.com>
2020-02-23 08:46:20 -0500

    Define Applicative & Monad instances for Surface.Term.

--- src/S/Surface.hs
+++ src/S/Surface.hs
@@ -3,6 +3,8 @@ module S.Surface
 ( Term(..)
 ) where
 
+import Control.Monad (ap)
+import Control.Monad.Trans.Class
 import S.Scope
 
 data Term a
@@ -12,5 +14,17 @@ data Term a
   | Type
   | Pi (Term a) (Scope Term a)
   deriving (Foldable, Functor, Traversable)
+
+instance Applicative Term where
+  pure = Var
+  (<*>) = ap
+
+instance Monad Term where
+  t >>= f = case t of
+    Var a  -> f a
+    Abs b  -> Abs (b >>= lift . f)
+    g :$ a -> (g >>= f) :$ (a >>= f)
+    Type   -> Type
+    Pi t b -> Pi (t >>= f) (b >>= lift . f)
 
 infixl 9 :$
