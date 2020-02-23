97aba8ee958e2531ff58ae90e305de7a2e4c805b
Rob Rix <rob.rix@me.com>
2020-02-23 09:22:50 -0500

    Stub in check & infer functions.

--- src/S/Elab.hs
+++ src/S/Elab.hs
@@ -1,2 +1,15 @@
+{-# LANGUAGE TypeOperators #-}
 module S.Elab
-() where
+( check
+, infer
+) where
+
+import qualified S.Core as Core
+import qualified S.Problem as Problem
+import           S.Syntax
+
+check :: MonadFail m => Problem.Term a -> Core.Term a -> m (Core.Term a ::: Core.Term a)
+check _ _ = fail "unimplemented"
+
+infer :: MonadFail m => Problem.Term a -> m (Core.Term a ::: Core.Term a)
+infer _ = fail "unimplemented"
