888bed4b7373124604e201f2ccf5ba18b786d56c
Rob Rix <rob.rix@me.com>
2020-02-24 16:01:13 -0500

    Stub in a Def instance for CheckC.

--- src/S/Syntax/Check.hs
+++ src/S/Syntax/Check.hs
@@ -1,5 +1,14 @@
+{-# LANGUAGE DataKinds #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
 module S.Syntax.Check
 ( CheckC(..)
 ) where
 
+import S.Syntax
+import S.Syntax.Classes
+
 newtype CheckC a = CheckC { runCheckC :: () }
+
+instance Def CheckC (Fin 'Z) CheckC where
+  def _ = CheckC ()
