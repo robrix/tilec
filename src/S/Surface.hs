29210c1f0c0c0ce4cfd1dd90d32840297a4c2a01
Rob Rix <rob.rix@me.com>
2020-02-24 14:59:49 -0500

    Move the syntax classes into their own module.

--- src/S/Surface.hs
+++ src/S/Surface.hs
@@ -1,18 +1,8 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DeriveTraversable #-}
-{-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE TypeOperators #-}
-{-# LANGUAGE UndecidableInstances #-}
 module S.Surface
 ( Term(..)
-, Var(..)
-, Let(..)
-, Lam(..)
-, Type(..)
-, (-->)
-, Prob(..)
-, Def(..)
 ) where
 
 import Bound.Class
@@ -21,7 +11,6 @@ import Control.Monad (ap)
 import Data.Functor.Classes
 import Data.Functor.Classes.Generic
 import GHC.Generics (Generic1)
-import S.Syntax
 
 data Term a
   = Var a
@@ -50,36 +39,3 @@ instance Monad Term where
     Pi t b    -> Pi (t >>= f) (b >>>= f)
 
 infixl 9 :$
-
-
-class Var expr a where
-  var :: a -> expr a
-
-class Var expr a => Let expr a where
-  let' :: expr a ::: expr a -> (a -> expr a) -> expr a
-
-class Var expr a => Lam expr a where
-  lam :: (a -> expr a) -> expr a
-  ($$) :: expr a -> expr a -> expr a
-
-  infixl 9 $$
-
-class Var expr a => Type expr a where
-  type' :: expr a
-  pi' :: expr a -> (a -> expr a) -> expr a
-
-  infixr 0 `pi'`
-
-(-->) :: Type expr a => expr a -> expr a -> expr a
-a --> b = a `pi'` const b
-
-infixr 0 -->
-
-class Var expr a => Prob expr a where
-  ex :: expr a -> (a -> expr a) -> expr a
-  (===) :: expr a -> expr a -> expr a
-
-  infixl 4 ===
-
-class Def expr a def | def -> expr where
-  def :: expr a ::: expr a -> def a
--- src/S/Syntax/Classes.hs
+++ src/S/Syntax/Classes.hs
@@ -1,2 +1,45 @@
+{-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE TypeOperators #-}
 module S.Syntax.Classes
-() where
+( Var(..)
+, Let(..)
+, Lam(..)
+, Type(..)
+, (-->)
+, Prob(..)
+, Def(..)
+) where
+
+import S.Syntax
+
+class Var expr a where
+  var :: a -> expr a
+
+class Var expr a => Let expr a where
+  let' :: expr a ::: expr a -> (a -> expr a) -> expr a
+
+class Var expr a => Lam expr a where
+  lam :: (a -> expr a) -> expr a
+  ($$) :: expr a -> expr a -> expr a
+
+  infixl 9 $$
+
+class Var expr a => Type expr a where
+  type' :: expr a
+  pi' :: expr a -> (a -> expr a) -> expr a
+
+  infixr 0 `pi'`
+
+(-->) :: Type expr a => expr a -> expr a -> expr a
+a --> b = a `pi'` const b
+
+infixr 0 -->
+
+class Var expr a => Prob expr a where
+  ex :: expr a -> (a -> expr a) -> expr a
+  (===) :: expr a -> expr a -> expr a
+
+  infixl 4 ===
+
+class Def expr a def | def -> expr where
+  def :: expr a ::: expr a -> def a
