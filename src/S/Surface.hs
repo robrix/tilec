fdadcd708d9396c8a4fa9813e149ff44b5853019
Rob Rix <rob.rix@me.com>
2020-02-24 11:32:35 -0500

    Define a class encoding typed definitions.

--- src/S/Surface.hs
+++ src/S/Surface.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE DeriveGeneric #-}
 {-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE FunctionalDependencies #-}
 {-# LANGUAGE TypeOperators #-}
 module S.Surface
 ( Term(..)
@@ -7,6 +8,7 @@ module S.Surface
 , Lam(..)
 , Type(..)
 , (-->)
+, Def(..)
 ) where
 
 import Bound.Class
@@ -66,3 +68,6 @@ class Bind expr => Type expr where
 a --> b = a `pi'` const b
 
 infixr 0 -->
+
+class Def expr def | def -> expr where
+  def :: expr a ::: expr a -> def a
