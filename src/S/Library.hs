61c430da88dd1eee7806eabf7a9102a30ffa479a
Rob Rix <rob.rix@me.com>
2020-02-24 15:02:32 -0500

    Define true & false constructors.

--- src/S/Library.hs
+++ src/S/Library.hs
@@ -1,6 +1,8 @@
 module S.Library
 ( -- * Booleans
   bool
+, true
+, false
 ) where
 
 import S.Syntax.Classes
@@ -8,3 +10,8 @@ import S.Syntax.Classes
 bool :: Type expr a => expr a
 bool = type' `pi'` \ a -> var a --> var a --> var a
 
+true :: Lam expr a => expr a
+true = lam (lam . const . var)
+
+false :: Lam expr a => expr a
+false = lam (const (lam var))
