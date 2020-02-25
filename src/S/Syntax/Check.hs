c69857c9fe9d26e5d5867ea158a325118dd762c8
Rob Rix <rob.rix@me.com>
2020-02-25 09:57:31 -0500

    Infer the types of variables.

--- src/S/Syntax/Check.hs
+++ src/S/Syntax/Check.hs
@@ -1,10 +1,15 @@
 {-# LANGUAGE DataKinds #-}
+{-# LANGUAGE GADTs #-}
+{-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeOperators #-}
 module S.Syntax.Check
 ( CheckC(..)
+, InferC(..)
 ) where
 
+import S.Context
 import S.Syntax
 import S.Syntax.Classes
 
@@ -12,3 +17,10 @@ newtype CheckC a = CheckC { runCheckC :: () }
 
 instance Def CheckC CheckC (Fin 'Z) CheckC where
   def _ = CheckC ()
+
+
+data InferC tm ty m a where
+  InferC :: { runInferC :: Ctx tm ty n -> m (tm (Fin n) ::: ty (Fin n)) } -> InferC tm ty m (Fin n)
+
+instance (Var tm (Fin n), Applicative m, Functor tm, Functor ty) => Var (InferC tm ty m) (Fin n) where
+  var n = InferC $ \ ctx -> let _ ::: ty = ctx ! n in pure (var n ::: ty)
