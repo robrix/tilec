be6bbe05a1554401c419cc6c862568d5d1166e41
Rob Rix <rob.rix@me.com>
2020-02-25 11:38:34 -0500

    Simplify the inference/checking stuff.

--- src/S/Syntax/Elab.hs
+++ src/S/Syntax/Elab.hs
@@ -1,7 +1,4 @@
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE GADTs #-}
 {-# LANGUAGE FlexibleContexts #-}
-{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeOperators #-}
 module S.Syntax.Elab
@@ -9,16 +6,12 @@ module S.Syntax.Elab
 , InferC(..)
 ) where
 
-import S.Context
 import S.Syntax
+import S.Syntax.Free
 import S.Syntax.Classes
 
-data CheckC tm ty m a where
-  CheckC :: { runCheckC :: Ctx tm ty n -> m (tm (Fin n)) } -> CheckC tm ty m (Fin n)
+newtype CheckC t m a = CheckC { runCheckC :: Term a a -> [t a] -> m (t a ::: t a) }
+newtype InferC t m a = InferC { runInferC ::             [t a] -> m (t a ::: t a) }
 
-
-data InferC tm ty m a where
-  InferC :: { runInferC :: Ctx tm ty n -> m (tm (Fin n) ::: ty (Fin n)) } -> InferC tm ty m (Fin n)
-
-instance (Var tm (Fin n), Applicative m, Functor tm, Functor ty) => Var (InferC tm ty m) (Fin n) where
-  var n = InferC $ \ ctx -> let _ ::: ty = ctx ! n in pure (var n ::: ty)
+instance (Var t Int, Applicative m) => Var (InferC t m) Int where
+  var n = InferC $ \ ctx -> pure (var n ::: ctx !! n)
