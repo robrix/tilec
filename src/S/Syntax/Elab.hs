d3e7708b482dac2b5f6308497ac2511835a100ee
Rob Rix <rob.rix@me.com>
2020-02-25 21:30:16 -0500

    Elaborate let bindings, I guess?

--- src/S/Syntax/Elab.hs
+++ src/S/Syntax/Elab.hs
@@ -1,17 +1,27 @@
 {-# LANGUAGE FlexibleContexts #-}
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
 module S.Syntax.Elab
-( CheckC(..)
-, InferC(..)
+( ElabC(..)
 ) where
 
+import Data.Maybe (fromMaybe)
 import S.Syntax
-import S.Syntax.Free
 import S.Syntax.Classes
 
-newtype CheckC t = CheckC { runCheckC :: Term Int Int -> [t] -> t }
-newtype InferC t = InferC { runInferC ::                 [t] -> t ::: t }
+newtype ElabC t = ElabC { runElabC :: Spine t -> t ::: t }
 
-instance Var Int t => Var Int (InferC t) where
-  var n = InferC $ \ ctx -> var n ::: ctx !! n
+instance (Var Int t, Err t) => Var Int (ElabC t) where
+  var n = ElabC $ \ ctx ->
+    var n ::: fromMaybe (err ("free variable: " <> show n)) (ctx !? n)
+
+instance (Lam Int t, Let Int t, Prob Int t, Type Int t, Err t) => Let Int (ElabC t) where
+  let' (tm ::: ty) b = ElabC $ \ ctx ->
+    let ty' = elab ctx ty === type'
+        tm' = elab ctx tm === ty'
+    in let' (tm' ::: ty') (elab (ctx :> term_ ty') . b)
+
+elab :: Spine t -> ElabC t -> t ::: t
+elab = flip runElabC
