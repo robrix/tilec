be574da27a522a3ce5f073c2bd3d27e035641047
Rob Rix <rob.rix@me.com>
2020-02-26 16:16:02 -0500

    Define instances of the syntax classes for pairs.

--- src/S/Syntax/Classes.hs
+++ src/S/Syntax/Classes.hs
@@ -18,12 +18,18 @@ import S.Syntax
 class Var a expr | expr -> a where
   var :: a -> expr
 
+instance (Var a expr1, Var a expr2) => Var a (expr1, expr2) where
+  var a = (var a, var a)
+
 instance (Var a expr1, Var a expr2) => Var a (expr1 ::: expr2) where
   var a = var a ::: var a
 
 
 class Var a expr => Let a expr where
   let' :: expr ::: expr -> (a -> expr) -> expr
+
+instance (Let a expr1, Let a expr2) => Let a (expr1, expr2) where
+  let' ((tm1, tm2) ::: (ty1, ty2)) b = (let' (tm1 ::: ty1) (fst . b), let' (tm2 ::: ty2) (snd . b))
 
 instance (Let a expr1, Let a expr2) => Let a (expr1 ::: expr2) where
   let' ((tm1 ::: tm2) ::: (ty1 ::: ty2)) b = let' (tm1 ::: ty1) (term_ . b) ::: let' (tm2 ::: ty2) (type_ . b)
@@ -35,6 +41,10 @@ class Var a expr => Lam a expr where
 
   infixl 9 $$
 
+instance (Lam a expr1, Lam a expr2) => Lam a (expr1, expr2) where
+  lam b = (lam (fst . b), lam (snd . b))
+  (f1, f2) $$ (a1, a2) = (f1 $$ a1, f2 $$ a2)
+
 instance (Lam a expr1, Lam a expr2) => Lam a (expr1 ::: expr2) where
   lam b = lam (term_ . b) ::: lam (type_ . b)
   (f1 ::: f2) $$ (a1 ::: a2) = f1 $$ a1 ::: f2 $$ a2
@@ -45,6 +55,10 @@ class Var a expr => Type a expr where
   pi' :: expr -> (a -> expr) -> expr
 
   infixr 0 `pi'`
+
+instance (Type a expr1, Type a expr2) => Type a (expr1, expr2) where
+  type' = (type', type')
+  pi' (t1, t2) b = (pi' t1 (fst . b), pi' t2 (snd . b))
 
 instance (Type a expr1, Type a expr2) => Type a (expr1 ::: expr2) where
   type' = type' ::: type'
@@ -62,6 +76,10 @@ class Var a expr => Prob a expr where
 
   infixl 4 ===
 
+instance (Prob a expr1, Prob a expr2) => Prob a (expr1, expr2) where
+  ex (t1, t2) b = (ex t1 (fst . b), ex t2 (snd . b))
+  (tm1, ty1) === (tm2, ty2) = (tm1 === tm2, ty1 === ty2)
+
 instance (Prob a expr1, Prob a expr2) => Prob a (expr1 ::: expr2) where
   ex (t1 ::: t2) b = ex t1 (term_ . b) ::: ex t2 (type_ . b)
   (tm1 ::: ty1) === (tm2 ::: ty2) = tm1 === tm2 ::: ty1 === ty2
@@ -69,6 +87,9 @@ instance (Prob a expr1, Prob a expr2) => Prob a (expr1 ::: expr2) where
 
 class Err expr where
   err :: String -> expr
+
+instance (Err expr1, Err expr2) => Err (expr1, expr2) where
+  err s = (err s, err s)
 
 instance (Err expr1, Err expr2) => Err (expr1 ::: expr2) where
   err s = err s ::: err s
