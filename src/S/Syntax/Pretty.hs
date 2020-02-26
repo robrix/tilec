c667e2f28d38728a387cbc419181798df535cce0
Rob Rix <rob.rix@me.com>
2020-02-26 17:11:25 -0500

    Pretty-print in colour.

--- src/S/Syntax/Pretty.hs
+++ src/S/Syntax/Pretty.hs
@@ -1,5 +1,6 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TupleSections #-}
 module S.Syntax.Pretty
@@ -19,16 +20,21 @@ import           System.Console.Terminal.Size as Size
 import           System.IO (stdout)
 
 prettyPrint :: MonadIO m => PrettyC -> m ()
-prettyPrint (PrettyC run) = putDoc (snd (run (Last 0)))
+prettyPrint (PrettyC run) = putDoc (PP.reAnnotate style (snd (run (Last 0)))) where
+  style = \case
+    Var -> mempty
+    Op -> ANSI.color ANSI.Cyan
+    Type -> ANSI.color ANSI.Yellow
+    Keyword -> ANSI.color ANSI.Magenta
 
-newtype PrettyC = PrettyC { runPrettyC :: Last Int -> (Last Int, PP.Doc ANSI.AnsiStyle) }
+newtype PrettyC = PrettyC { runPrettyC :: Last Int -> (Last Int, PP.Doc Highlight) }
   deriving (Semigroup)
 
 instance Show PrettyC where
   showsPrec p (PrettyC run) = showsPrec p (snd (run (Last 0)))
 
 instance Var Int PrettyC where
-  var = word . (PP.pretty '_' <>) . PP.pretty
+  var = highlight Var . (PP.pretty '_' <>) . PP.pretty
 
 instance Let Int PrettyC where
   let' (tm ::: ty) b = fresh (\ v -> kw "let" <+> var v <+> op "=" <+> tm <+> op ":" <+> ty <+> kw "in" <+> b v)
@@ -38,7 +44,7 @@ instance Lam Int PrettyC where
   f $$ a = f <+> a
 
 instance Type Int PrettyC where
-  type' = kw "Type"
+  type' = highlight Type (PP.pretty "Type")
   pi' t f = fresh $ \ v -> parens (var v <+> op ":" <+> t) <+> op "->" <+> f v
 
 
@@ -49,17 +55,17 @@ data Highlight
   | Keyword
   deriving (Eq, Ord, Show)
 
-word :: PP.Doc ANSI.AnsiStyle -> PrettyC
-word s = PrettyC (, s)
+highlight :: Highlight -> PP.Doc Highlight -> PrettyC
+highlight h s = PrettyC (, PP.annotate h s)
 
 kw :: String -> PrettyC
-kw = word . PP.pretty
+kw = highlight Keyword . PP.pretty
 
 op :: String -> PrettyC
-op = word . PP.pretty
+op = highlight Op . PP.pretty
 
 (<+>) :: PrettyC -> PrettyC -> PrettyC
-l <+> r = l <> word (PP.pretty ' ') <> r
+l <+> r = l <> PrettyC (, PP.pretty ' ') <> r
 
 infixr 6 <+>
 
