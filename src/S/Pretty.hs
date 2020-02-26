d0463e034174c81a152aa2b4ab2d18c8b2d071b9
Rob Rix <rob.rix@me.com>
2020-02-26 17:46:23 -0500

    Derive an Applicative instance for Rainbow.

--- src/S/Pretty.hs
+++ src/S/Pretty.hs
@@ -44,4 +44,4 @@ rainbow :: Rainbow doc -> doc
 rainbow = (`runRainbow` 0)
 
 newtype Rainbow doc = Rainbow { runRainbow :: Int -> doc }
-  deriving (Functor, Monoid, Semigroup)
+  deriving (Applicative, Functor, Monoid, Semigroup)
