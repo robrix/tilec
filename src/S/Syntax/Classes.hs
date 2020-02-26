68a53d008febd4e4bf8ba990e7e59efc92f640f4
Rob Rix <rob.rix@me.com>
2020-02-26 16:17:37 -0500

    Derive the syntax instances for :::.

--- src/S/Syntax/Classes.hs
+++ src/S/Syntax/Classes.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
 module S.Syntax.Classes
@@ -21,8 +23,7 @@ class Var a expr | expr -> a where
 instance (Var a expr1, Var a expr2) => Var a (expr1, expr2) where
   var a = (var a, var a)
 
-instance (Var a expr1, Var a expr2) => Var a (expr1 ::: expr2) where
-  var a = var a ::: var a
+deriving instance (Var a expr1, Var a expr2) => Var a (expr1 ::: expr2)
 
 
 class Var a expr => Let a expr where
@@ -31,8 +32,7 @@ class Var a expr => Let a expr where
 instance (Let a expr1, Let a expr2) => Let a (expr1, expr2) where
   let' ((tm1, tm2) ::: (ty1, ty2)) b = (let' (tm1 ::: ty1) (fst . b), let' (tm2 ::: ty2) (snd . b))
 
-instance (Let a expr1, Let a expr2) => Let a (expr1 ::: expr2) where
-  let' ((tm1 ::: tm2) ::: (ty1 ::: ty2)) b = let' (tm1 ::: ty1) (term_ . b) ::: let' (tm2 ::: ty2) (type_ . b)
+deriving instance (Let a expr1, Let a expr2) => Let a (expr1 ::: expr2)
 
 
 class Var a expr => Lam a expr where
@@ -45,9 +45,7 @@ instance (Lam a expr1, Lam a expr2) => Lam a (expr1, expr2) where
   lam b = (lam (fst . b), lam (snd . b))
   (f1, f2) $$ (a1, a2) = (f1 $$ a1, f2 $$ a2)
 
-instance (Lam a expr1, Lam a expr2) => Lam a (expr1 ::: expr2) where
-  lam b = lam (term_ . b) ::: lam (type_ . b)
-  (f1 ::: f2) $$ (a1 ::: a2) = f1 $$ a1 ::: f2 $$ a2
+deriving instance (Lam a expr1, Lam a expr2) => Lam a (expr1 ::: expr2)
 
 
 class Var a expr => Type a expr where
@@ -60,9 +58,7 @@ instance (Type a expr1, Type a expr2) => Type a (expr1, expr2) where
   type' = (type', type')
   pi' (t1, t2) b = (pi' t1 (fst . b), pi' t2 (snd . b))
 
-instance (Type a expr1, Type a expr2) => Type a (expr1 ::: expr2) where
-  type' = type' ::: type'
-  pi' (t1 ::: t2) b = pi' t1 (term_ . b) ::: pi' t2 (type_ . b)
+deriving instance (Type a expr1, Type a expr2) => Type a (expr1 ::: expr2)
 
 (-->) :: Type a expr => expr -> expr -> expr
 a --> b = a `pi'` const b
@@ -80,9 +76,7 @@ instance (Prob a expr1, Prob a expr2) => Prob a (expr1, expr2) where
   ex (t1, t2) b = (ex t1 (fst . b), ex t2 (snd . b))
   (tm1, ty1) === (tm2, ty2) = (tm1 === tm2, ty1 === ty2)
 
-instance (Prob a expr1, Prob a expr2) => Prob a (expr1 ::: expr2) where
-  ex (t1 ::: t2) b = ex t1 (term_ . b) ::: ex t2 (type_ . b)
-  (tm1 ::: ty1) === (tm2 ::: ty2) = tm1 === tm2 ::: ty1 === ty2
+deriving instance (Prob a expr1, Prob a expr2) => Prob a (expr1 ::: expr2)
 
 
 class Err expr where
@@ -91,8 +85,7 @@ class Err expr where
 instance (Err expr1, Err expr2) => Err (expr1, expr2) where
   err s = (err s, err s)
 
-instance (Err expr1, Err expr2) => Err (expr1 ::: expr2) where
-  err s = err s ::: err s
+deriving instance (Err expr1, Err expr2) => Err (expr1 ::: expr2)
 
 
 class Def tm ty a def | def -> tm ty where
