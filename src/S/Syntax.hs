5bbd80a018e444b008ffd338c549cc9a4c564e69
Rob Rix <rob.rix@me.com>
2020-02-27 02:19:38 -0500

    :fire: :=.

--- src/S/Syntax.hs
+++ src/S/Syntax.hs
@@ -7,8 +7,6 @@ module S.Syntax
 , pattern (:::)
 , term_
 , type_
-, (:=)(..)
-, pattern (:=)
 , Stack(..)
 , (!?)
 ) where
@@ -31,20 +29,6 @@ term_ (a ::: _) = a
 
 type_ :: a ::: b -> b
 type_ (_ ::: b) = b
-
-
-newtype a := b = Define (a, b)
-  deriving (Eq, Foldable, Functor, Ord, Traversable)
-
-instance (Show a, Show b) => Show (a := b) where
-  showsPrec p (a := b) = showParen (p > 0) (shows a . showString " := " . showsPrec 1 b)
-
-pattern (:=) :: a -> b -> a := b
-pattern a := b = Define (a, b)
-
-{-# COMPLETE (:=) #-}
-
-infixl 0 :=
 
 
 data Stack a
