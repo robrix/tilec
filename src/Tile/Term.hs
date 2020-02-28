cda2db81068bd741486cdec766a2acfd482fc4df
Rob Rix <rob.rix@me.com>
2020-02-28 18:58:06 -0500

    Define an Eq instance for Term.

--- src/Tile/Term.hs
+++ src/Tile/Term.hs
@@ -23,6 +23,20 @@ data Term v a
   | (Term v a ::: Term v a) :===: (Term v a ::: Term v a)
   | Err String
 
+instance (Eq a, Num v) => Eq (Term v a) where
+  (==) = eq 0 where
+    eq n l r = case (l, r) of
+      (Var v1, Var v2) -> v1 == v2
+      (Let v1 b1, Let v2 b2) -> eq n v1 v2 && eq (n + 1) (b1 n) (b2 n)
+      (Lam p1 b1, Lam p2 b2) -> p1 == p2 && eq (n + 1) (b1 n) (b2 n)
+      (f1 :$ a1, f2 :$ a2) -> eq n f1 f2 && eq n a1 a2
+      (Type, Type) -> True
+      ((p1, a1) :-> b1, (p2, a2) :-> b2) -> p1 == p2 && eq n a1 a2 && eq (n + 1) (b1 n) (b2 n)
+      (E t1 b1, E t2 b2) -> eq n t1 t2 && eq (n + 1) (b1 n) (b2 n)
+      ((m11 ::: t11) :===: (m21 ::: t21), (m12 ::: t12) :===: (m22 ::: t22)) -> eq n m11 m12 && eq n t11 t12 && eq n m21 m22 && eq n t21 t22
+      (Err s1, Err s2) -> s1 == s2
+      _ -> False
+
 instance Functor (Term v) where
   fmap f = go where
     go = \case
