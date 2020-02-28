0a5b2ea37655b398ec65af3b04455096b10ba00e
Rob Rix <rob.rix@me.com>
2020-02-28 13:29:29 -0500

    Elaborate with respect to a goal.

--- src/Tile/Elab.hs
+++ src/Tile/Elab.hs
@@ -1,6 +1,6 @@
+{-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE FlexibleContexts #-}
 {-# LANGUAGE FlexibleInstances #-}
-{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE TypeOperators #-}
 {-# LANGUAGE UndecidableInstances #-}
@@ -18,43 +18,62 @@ import Data.Maybe (fromMaybe)
 import Tile.Syntax
 import Tile.Type
 
-newtype Elab v t b = Elab { runElab :: Map v t -> b }
-  deriving (Applicative, Functor, Monad)
+elab :: Elab v t b ::: t -> Map v t -> b
+elab (m ::: t) = runElab m t
+
+newtype Elab v t b = Elab { runElab :: t -> Map v t -> b }
+  deriving (Functor)
 
 instance (Ord v, Show v, Prob v t, Type v t, Err t) => Var v (Elab v t t) where
-  var n = Elab $
-    type' `ex` \ _A ->
-    var _A `ex` \ v ->
+  var n = Elab $ \ t ->
+    pure t `ex` \ v ->
     (var n ::: typeOf n)
     ===
-    (var v ::: var _A)
+    (var v ::: pure t)
 
 instance (Ord v, Show v, Let v t, Prob v t, Type v t, Err t) => Let v (Elab v t t) where
-  let' tm b = Elab $
+  let' tm b = Elab $ \ t ->
     type' `ex` \ _A ->
-    let' (runElab tm .:. var _A) (var _A |- runElab . b)
+    type' `ex` \ _B ->
+    pure t `ex` \ v ->
+    (let' (elab (tm ::: var _A)) (var _A |- elab . (::: var _B) . b) ::: var _B)
+    ===
+    (var v ::: pure t)
 
 instance (Ord v, Show v, Let v t, Lam v t, Prob v t, Type v t, Err t) => Lam v (Elab v t t) where
-  lam b = Elab $
+  lam b = Elab $ \ t ->
     type' `ex` \ _A ->
     type' `ex` \ _B ->
-    lam (var _A |- runElab . b) .:. (var _A --> var _B)
-
-  f $$ a = Elab $
-    type' `ex` \ _A ->
-    type' `ex` \ _B ->
-    var _B `ex` \ res ->
-    let' (runElab a) $ \ a' ->
-    (runElab f $$ var a' ::: (var _A --> var _B) $$ var a')
+    pure t `ex` \ v ->
+    (lam (var _A |- elab . (::: var _B) . b) ::: (var _A --> var _B))
     ===
-    (var res ::: var _B)
+    (var v ::: pure t)
+
+  f $$ a = Elab $ \ t ->
+    type' `ex` \ _A ->
+    type' `ex` \ _B ->
+    pure t `ex` \ v ->
+    let' (elab (a ::: var _A)) $ \ a' ->
+    let' (var _A --> var _B) $ \ f' ->
+    (elab (f ::: var f') $$ var a' ::: var f' $$ var a')
+    ===
+    (var v ::: pure t)
 
 instance (Ord v, Show v, Let v t, Prob v t, Type v t, Err t) => Type v (Elab v t t) where
-  type' = Elab type'
+  type' = Elab $ \ t ->
+    pure t `ex` \ v ->
+    (type' ::: type')
+    ===
+    (var v ::: pure t)
 
-  a >-> b = Elab .
-    let' (runElab a) $ \ a' ->
-    (var a' .:. type') >-> var a' |- runElab . b
+  a >-> b = Elab $ \ t ->
+    type' `ex` \ _A ->
+    (var _A --> type') `ex` \ _B ->
+    pure t `ex` \ v ->
+    let' (elab (a ::: type')) $ \ a' ->
+    ((var a' >-> var a' |- elab . (::: var _B) . b) ::: type')
+    ===
+    (var v ::: pure t)
 
   tm .:. ty = Elab (runElab tm .:. runElab ty .:. type')
 
