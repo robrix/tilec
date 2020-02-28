12e72f255bd2de7e9e3f44359ba55e569be2df00
Rob Rix <rob.rix@me.com>
2020-02-28 18:24:31 -0500

    Add equations to Term.

--- src/Tile/Term.hs
+++ src/Tile/Term.hs
@@ -2,6 +2,7 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE LambdaCase #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE TypeOperators #-}
 module Tile.Term
 ( Term(..)
 , interpret
@@ -18,6 +19,7 @@ data Term a b
   | Type
   | (Plicit, Term a b) :-> (a -> Term a b)
   | E (Term a b) (a -> Term a b)
+  | (Term a b, Term a b) :===: (Term a b, Term a b)
   | Err String
   deriving (Functor)
 
@@ -27,14 +29,15 @@ instance Applicative (Term a) where
 
 instance Monad (Term a) where
   t >>= f = case t of
-    Var a   -> f a
-    Let v b -> Let (v >>= f) (b >=> f)
-    Lam p b -> Lam p (b >=> f)
-    g :$ a  -> (g >>= f) :$ (a >>= f)
-    Type    -> Type
-    t :-> b -> fmap (>>= f) t :-> (b >=> f)
-    E t b   -> E (t >>= f) (b >=> f)
-    Err s   -> Err s
+    Var a                   -> f a
+    Let v b                 -> Let (v >>= f) (b >=> f)
+    Lam p b                 -> Lam p (b >=> f)
+    g :$ a                  -> (g >>= f) :$ (a >>= f)
+    Type                    -> Type
+    t :-> b                 -> fmap (>>= f) t :-> (b >=> f)
+    E t b                   -> E (t >>= f) (b >=> f)
+    (m1, t1) :===: (m2, t2) -> (m1 >>= f, t1 >>= f) :===: (m2 >>= f, t2 >>= f)
+    Err s                   -> Err s
 
 instance Var a (Term a a) where
   var = Var
@@ -55,14 +58,16 @@ instance Err (Term a a) where
 
 infixl 9 :$
 infixr 0 :->
+infixl 4 :===:
 
 interpret :: (Let a t, Lam a t, Type a t, Prob a t, Err t) => Term a a -> t
 interpret = \case
-  Var v   -> var v
-  Let v b -> let' (interpret v) (interpret . b)
-  Lam p b -> lam p (interpret . b)
-  f :$ a  -> interpret f $$ interpret a
-  Type    -> type'
-  t :-> b -> fmap interpret t >-> interpret . b
-  E t b   -> interpret t `ex` interpret . b
-  Err s   -> err s
+  Var v                   -> var v
+  Let v b                 -> let' (interpret v) (interpret . b)
+  Lam p b                 -> lam p (interpret . b)
+  f :$ a                  -> interpret f $$ interpret a
+  Type                    -> type'
+  t :-> b                 -> fmap interpret t >-> interpret . b
+  E t b                   -> interpret t `ex` interpret . b
+  (m1, t1) :===: (m2, t2) -> (interpret m1 ::: interpret t1) === (interpret m2 ::: interpret t2)
+  Err s                   -> err s
