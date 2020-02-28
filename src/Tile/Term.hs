2b58073c0408b1ad4e3b9d21a0a2e38f1394848d
Rob Rix <rob.rix@me.com>
2020-02-28 18:14:48 -0500

    Revert ":fire: Tile.Term."
    This reverts commit 374aa76a937b13b643112fb3d0e0d41b9566ecfa.

---
+++ src/Tile/Term.hs
@@ -0,0 +1,65 @@
+{-# LANGUAGE DeriveFunctor #-}
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+module Tile.Term
+( Term(..)
+, interpret
+) where
+
+import Control.Monad (ap, (>=>))
+import Tile.Syntax
+
+data Term a b
+  = Var b
+  | Let (Term a b) (a -> Term a b)
+  | Lam Plicit (a -> Term a b)
+  | Term a b :$ Term a b
+  | Type
+  | (Plicit, Term a b) :-> (a -> Term a b)
+  | Err String
+  deriving (Functor)
+
+instance Applicative (Term a) where
+  pure = Var
+  (<*>) = ap
+
+instance Monad (Term a) where
+  t >>= f = case t of
+    Var a   -> f a
+    Let v b -> Let (v >>= f) (b >=> f)
+    Lam p b -> Lam p (b >=> f)
+    g :$ a  -> (g >>= f) :$ (a >>= f)
+    Type    -> Type
+    t :-> b -> fmap (>>= f) t :-> (b >=> f)
+    Err s   -> Err s
+
+instance Var a (Term a a) where
+  var = Var
+
+instance Let a (Term a a) where
+  let' = Let
+
+instance Lam a (Term a a) where
+  lam = Lam
+  ($$) = (:$)
+
+instance Type a (Term a a) where
+  type' = Type
+  (>->) = (:->)
+
+instance Err (Term a a) where
+  err = Err
+
+infixl 9 :$
+infixr 0 :->
+
+interpret :: (Let a t, Lam a t, Type a t, Err t) => Term a a -> t
+interpret = \case
+  Var v   -> var v
+  Let v b -> let' (interpret v) (interpret . b)
+  Lam p b -> lam p (interpret . b)
+  f :$ a  -> interpret f $$ interpret a
+  Type    -> type'
+  t :-> b -> fmap interpret t >-> interpret . b
+  Err s   -> err s
