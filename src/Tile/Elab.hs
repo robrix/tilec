91e7b3b86fd47b2fc453194a554613d571ee0f68
Rob Rix <rob.rix@me.com>
2020-02-28 16:02:51 -0500

    Run in Elab.

--- src/Tile/Elab.hs
+++ src/Tile/Elab.hs
@@ -53,18 +53,18 @@ instance (Ord v, Show v, Let v t, Prob v t, Type v t, Err t) => Type v (Elab v t
 deriving instance (Ord v, Show v, Prob v t, Err t) => Prob v (Elab v t t)
 
 
-elab :: Elab v t b ::: t -> ReaderC t (ReaderC (Map v t) Identity) b
-elab (m ::: t) = local (const t) (runElab m)
+elab :: Elab v t b ::: t -> Elab v t b
+elab (m ::: t) = Elab (local (const t) (runElab m))
 
-typeOf :: (Has (Reader (Map v t)) sig m, Ord v, Show v, Err (m t)) => v -> m t
-typeOf n = asks (!? n) >>= maybe (err ("free variable: " <> show n)) pure
+typeOf :: (Ord v, Show v, Err t) => v -> Elab v t t
+typeOf n = Elab (asks (!? n) >>= maybe (err ("free variable: " <> show n)) pure)
 
-(|-) :: (Has (Reader (Map v t)) sig m, Ord v) => v ::: m t -> m t -> m t
-(a ::: t) |- b = do
-  t' <- t
-  local (insert a t') b
+(|-) :: Ord v => v ::: Elab v t t -> Elab v t t -> Elab v t t
+(a ::: t) |- b = Elab $ do
+  t' <- runElab t
+  local (insert a t') (runElab b)
 
 infixl 0 |-
 
-check :: Prob v t => (ReaderC t (ReaderC (Map v t) Identity) t ::: ReaderC t (ReaderC (Map v t) Identity) t -> ReaderC t (ReaderC (Map v t) Identity) t) -> Elab v t t
-check f = Elab $ ask `ex` \ v -> f (var v ::: ask)
+check :: Prob v t => (Elab v t t ::: Elab v t t -> Elab v t t) -> Elab v t t
+check f = Elab $ ask `ex` \ v -> runElab (f (pure (var v) ::: Elab ask))
