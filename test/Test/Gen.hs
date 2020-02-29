4e5b32226ef5eb4765f01e18fa56bb98f6a18f18
Rob Rix <rob.rix@me.com>
2020-02-28 20:30:52 -0500

    Define a generator for Plicit.

--- test/Test/Gen.hs
+++ test/Test/Gen.hs
@@ -1,10 +1,13 @@
 module Test.Gen
 ( var
 , let'
+, plicit
 ) where
 
 import           Control.Monad.Trans.Reader
-import           Hedgehog (Gen)
+import           Hedgehog (Gen, MonadGen(..))
+import qualified Hedgehog.Gen as Gen
+import           Tile.Plicit
 import qualified Tile.Syntax as Syn
 
 var :: Syn.Var v t => ReaderT Int Gen v -> ReaderT Int Gen t
@@ -12,3 +15,6 @@ var v = Syn.var <$> v
 
 let' :: Syn.Let v t => ReaderT Int Gen t -> ReaderT Int Gen (v -> t) -> ReaderT Int Gen t
 let' t b = Syn.let' <$> t <*> b
+
+plicit :: MonadGen m => m Plicit
+plicit = Gen.enumBounded
