80c396d2ece78caaafeb9969e73ec42d61b9f35d
Rob Rix <rob.rix@me.com>
2020-02-29 01:18:50 -0500

    Label generated terms.

--- test/Test/Term.hs
+++ test/Test/Term.hs
@@ -1,8 +1,12 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE TupleSections #-}
 module Test.Term
 ( tests
 ) where
 
-import Control.Monad.Trans.Reader
+import Control.Monad.Reader
+import Data.Foldable (for_)
+import Data.Set
 import Hedgehog hiding (Gen)
 import Hedgehog.Gen as Gen
 import Test.Gen as Gen
@@ -13,14 +17,17 @@ import Tile.Term
 tests :: TestTree
 tests = testGroup "Term"
   [ testProperty "reflexivity of ==" . property $ do
-    t <- forAll (runReaderT (runGen term) 0)
+    (labels, t) <- forAll (runReaderT (runGen term) 0)
+    for_ labels label
     t === t
   ]
 
-term :: Gen (Term Int Int)
-term = runReaderT go (0 :: Int) where
+term :: Gen (Set LabelName, Term Int Int)
+term = go where
   go = ask >>= \ i -> recursive choice
-    (if i > 0 then [ Gen.localVar, Gen.type' ] else [ Gen.type' ])
-    [ Gen.subterm2 go (local succ go) (\ t b -> let' t (const b))
-    , Gen.subtermM (local succ go) (\ b -> lam <$> plicit <*> pure (const b))
+    ((if i > 0 then (tag "var" Gen.localVar :) else id) [ tag "type" Gen.type' ])
+    [ tag "let" $ Gen.subterm2 go' (local succ go') (\ t b -> let' t (const b))
+    , tag "lam" $ Gen.subtermM (local succ go') (\ b -> lam <$> plicit <*> pure (const b))
     ]
+  go' = snd <$> go
+  tag s = fmap (singleton s,)
