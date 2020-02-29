bd4ef37891848738da31378f69cefb813275c977
Rob Rix <rob.rix@me.com>
2020-02-28 20:51:44 -0500

    Increment the variable counter under the binder.

--- test/Test/Gen.hs
+++ test/Test/Gen.hs
@@ -1,9 +1,11 @@
+{-# LANGUAGE FlexibleContexts #-}
 module Test.Gen
 ( var
 , let'
 , plicit
 ) where
 
+import           Control.Monad.Reader
 import           Hedgehog (MonadGen(..))
 import qualified Hedgehog.Gen as Gen
 import           Tile.Plicit
@@ -12,8 +14,8 @@ import qualified Tile.Syntax as Syn
 var :: (Syn.Var v t, MonadGen m) => m v -> m t
 var v = Syn.var <$> v
 
-let' :: (Syn.Let v t, MonadGen m) => m t -> m (v -> t) -> m t
-let' t b = Syn.let' <$> t <*> b
+let' :: (Syn.Let v t, MonadGen m, MonadReader Int m) => m t -> m (v -> t) -> m t
+let' t b = Syn.let' <$> t <*> local succ b
 
 plicit :: MonadGen m => m Plicit
 plicit = Gen.enumBounded
