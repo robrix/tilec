96c291acd2cb758f64d2185db817395486436ac9
Rob Rix <rob.rix@me.com>
2020-02-29 02:14:59 -0500

    Rename some type variables.

--- test/Test/Gen.hs
+++ test/Test/Gen.hs
@@ -31,10 +31,10 @@ plicit = Gen.enumBounded
 var_ :: (Var Int t, MonadGen m, MonadReader Int m, MonadWriter (Set LabelName) m) => m t
 var_ = ask >>= \ i -> if i <= 0 then Gen.discard else var <$> Gen.int (Range.constant 0 i) <* tag "var"
 
-let_ :: (MonadGen m, MonadReader Int m, Let v a, MonadWriter (Set LabelName) m) => m a -> m a
+let_ :: (MonadGen m, MonadReader Int m, Let v t, MonadWriter (Set LabelName) m) => m t -> m t
 let_ t = Gen.subtermM2 t (local succ t) (\ v b -> let' v (const b) <$ tag "let")
 
-lam_ :: (MonadGen m, MonadReader Int m, Lam v a, MonadWriter (Set LabelName) m) => m a -> m a
+lam_ :: (MonadGen m, MonadReader Int m, Lam v t, MonadWriter (Set LabelName) m) => m t -> m t
 lam_ t = Gen.subtermM (local succ t) (\ b -> lam <$> plicit <*> pure (const b) <* tag "lam")
 
 app_ :: (Lam Int t, MonadGen m, MonadWriter (Set LabelName) m) => m t -> m t
@@ -43,13 +43,13 @@ app_ t = Gen.subtermM2 t t (\ f a -> f $$ a <$ tag "$$")
 type_ :: (Type Int t, MonadWriter (Set LabelName) m) => m t
 type_ = type' <$ tag "type"
 
-pi_ :: (MonadGen m, MonadReader Int m, Type v a, MonadWriter (Set LabelName) m) => m a -> m a
+pi_ :: (MonadGen m, MonadReader Int m, Type v t, MonadWriter (Set LabelName) m) => m t -> m t
 pi_ t = Gen.subtermM2 t (local succ t) (\ t b -> (>-> const b) . (, t) <$> plicit <* tag ">->")
 
-ex_ :: (MonadGen m, MonadReader Int m, Prob v a, MonadWriter (Set LabelName) m) => m a -> m a
+ex_ :: (MonadGen m, MonadReader Int m, Prob v t, MonadWriter (Set LabelName) m) => m t -> m t
 ex_ t = Gen.subtermM2 t (local succ t) (\ t b -> (t `ex` const b) <$ tag "ex")
 
-eq_ :: (MonadGen m, Prob v a, MonadWriter (Set LabelName) m) => m a -> m a
+eq_ :: (MonadGen m, Prob v t, MonadWriter (Set LabelName) m) => m t -> m t
 eq_ t = Gen.subtermM2 t t (\ m1 t1 -> Gen.subtermM2 t t (\ m2 t2 -> ((m1 ::: t1) === (m2 ::: t2)) <$ tag "==="))
 
 err_ :: (Err t, MonadGen m, MonadWriter (Set LabelName) m) => m t
