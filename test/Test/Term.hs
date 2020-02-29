d34091fdc5fc3615e20637592343425d1c8d2ccc
Rob Rix <rob.rix@me.com>
2020-02-29 01:26:10 -0500

    :fire: qualifiers.

--- test/Test/Term.hs
+++ test/Test/Term.hs
@@ -25,12 +25,12 @@ tests = testGroup "Term"
 term :: ReaderT Int Gen (Set LabelName, Term Int Int)
 term = go where
   go = ask >>= \ i -> recursive choice
-    ((if i > 0 then (tag "var" Gen.localVar :) else id) [ tag "type" (pure type') ])
-    [ tag "let" $ Gen.subterm2 go' (local succ go') (\ t b -> let' t (const b))
-    , tag "lam" $ Gen.subtermM (local succ go') (\ b -> lam <$> plicit <*> pure (const b))
-    , tag "$$"  $ Gen.subterm2 go' go' ($$)
-    , tag ">->" $ Gen.subtermM2 go' (local succ go') (\ t b -> (>-> const b) . (, t) <$> plicit)
-    , tag "ex"  $ Gen.subterm2 go' (local succ go') (\ t b -> t `ex` const b)
+    ((if i > 0 then (tag "var" localVar :) else id) [ tag "type" (pure type') ])
+    [ tag "let" $ subterm2 go' (local succ go') (\ t b -> let' t (const b))
+    , tag "lam" $ subtermM (local succ go') (\ b -> lam <$> plicit <*> pure (const b))
+    , tag "$$"  $ subterm2 go' go' ($$)
+    , tag ">->" $ subtermM2 go' (local succ go') (\ t b -> (>-> const b) . (, t) <$> plicit)
+    , tag "ex"  $ subterm2 go' (local succ go') (\ t b -> t `ex` const b)
     ]
   go' = snd <$> go
   tag s = fmap (singleton s,)
