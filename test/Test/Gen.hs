e64091a88b3f9284a2d8ea1de3c0f16c33afa7a8
Rob Rix <rob.rix@me.com>
2020-02-28 20:25:20 -0500

    Define a generator of variables.

--- test/Test/Gen.hs
+++ test/Test/Gen.hs
@@ -1,2 +1,10 @@
 module Test.Gen
-() where
+( var
+) where
+
+import           Control.Monad.Trans.Reader
+import           Hedgehog (Gen)
+import qualified Tile.Syntax as Syn
+
+var :: Syn.Var v t => ReaderT Int Gen v -> ReaderT Int Gen t
+var v = Syn.var <$> v
