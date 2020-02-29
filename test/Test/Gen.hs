7667818a623b04c1fe57d10b97c3bbf03056a462
Rob Rix <rob.rix@me.com>
2020-02-28 20:48:43 -0500

    Generalize var & let'.

--- test/Test/Gen.hs
+++ test/Test/Gen.hs
@@ -4,16 +4,15 @@ module Test.Gen
 , plicit
 ) where
 
-import           Control.Monad.Trans.Reader
-import           Hedgehog (Gen, MonadGen(..))
+import           Hedgehog (MonadGen(..))
 import qualified Hedgehog.Gen as Gen
 import           Tile.Plicit
 import qualified Tile.Syntax as Syn
 
-var :: Syn.Var v t => ReaderT Int Gen v -> ReaderT Int Gen t
+var :: (Syn.Var v t, MonadGen m) => m v -> m t
 var v = Syn.var <$> v
 
-let' :: Syn.Let v t => ReaderT Int Gen t -> ReaderT Int Gen (v -> t) -> ReaderT Int Gen t
+let' :: (Syn.Let v t, MonadGen m) => m t -> m (v -> t) -> m t
 let' t b = Syn.let' <$> t <*> b
 
 plicit :: MonadGen m => m Plicit
