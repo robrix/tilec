ae736676232b1244cd83790b643d92926bc9785a
Rob Rix <rob.rix@me.com>
2020-02-28 20:55:33 -0500

    Generalize some constraints.

--- test/Test/Gen.hs
+++ test/Test/Gen.hs
@@ -13,16 +13,16 @@ import qualified Hedgehog.Gen as Gen
 import           Tile.Plicit
 import qualified Tile.Syntax as Syn
 
-var :: (Syn.Var v t, MonadGen m) => m v -> m t
+var :: (Syn.Var v t, Functor m) => m v -> m t
 var v = Syn.var <$> v
 
-let' :: (Syn.Let v t, MonadGen m, MonadReader Int m) => m t -> m (v -> t) -> m t
+let' :: (Syn.Let v t, MonadReader Int m) => m t -> m (v -> t) -> m t
 let' t b = Syn.let' <$> t <*> local succ b
 
 lam :: (Syn.Lam v t, MonadGen m, MonadReader Int m) => m (v -> t) -> m t
 lam b = Syn.lam <$> plicit <*> local succ b
 
-($$) :: (Syn.Lam v t, MonadGen m) => m t -> m t -> m t
+($$) :: (Syn.Lam v t, Applicative m) => m t -> m t -> m t
 f $$ a = (Syn.$$) <$> f <*> a
 
 infixl 9 $$
