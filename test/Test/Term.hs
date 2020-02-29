2ace29ddc9ee922a4af852f334e9a4b613552ca1
Rob Rix <rob.rix@me.com>
2020-02-28 21:45:27 -0500

    Test the reflexivity of == for Term.

--- test/Test/Term.hs
+++ test/Test/Term.hs
@@ -2,8 +2,18 @@ module Test.Term
 ( tests
 ) where
 
+import Hedgehog
+import Test.Gen as Gen
 import Test.Tasty
+import Test.Tasty.Hedgehog
+import Tile.Term
 
 tests :: TestTree
 tests = testGroup "Term"
-  []
+  [ testProperty "reflexivity of ==" . property $ do
+    t <- forAll term
+    t === t
+  ]
+
+term :: Gen (Term Int Int)
+term = Gen.type'
