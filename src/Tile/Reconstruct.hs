a78e2f70d5c25f5c2155d55acf390f8c22efb371
Rob Rix <rob.rix@me.com>
2020-03-02 06:41:09 -0500

    Derive a Let instance for Reconstruct.

--- src/Tile/Reconstruct.hs
+++ src/Tile/Reconstruct.hs
@@ -1,5 +1,7 @@
 {-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE UndecidableInstances #-}
 module Tile.Reconstruct
 ( Reconstruct(..)
@@ -11,3 +13,5 @@ newtype Reconstruct t a = Reconstruct { runReconstruct :: a }
 
 instance Var v t => Var v (Reconstruct t t) where
   var = Reconstruct . var
+
+deriving instance Let v t => Let v (Reconstruct t t)
