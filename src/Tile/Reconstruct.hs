af53650211efff8b477c2e1605f12e871d370a8b
Rob Rix <rob.rix@me.com>
2020-03-01 23:36:30 -0500

    Define a Var instance for Reconstruct.

--- src/Tile/Reconstruct.hs
+++ src/Tile/Reconstruct.hs
@@ -1,5 +1,13 @@
+{-# LANGUAGE FlexibleInstances #-}
+{-# LANGUAGE MultiParamTypeClasses #-}
+{-# LANGUAGE UndecidableInstances #-}
 module Tile.Reconstruct
 ( Reconstruct(..)
 ) where
 
+import Tile.Syntax
+
 newtype Reconstruct t a = Reconstruct { runReconstruct :: a }
+
+instance Var v t => Var v (Reconstruct t t) where
+  var = Reconstruct . var
