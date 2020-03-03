1089cff362bc1b1be4561b4c341d48cb462dba33
Rob Rix <rob.rix@me.com>
2020-03-03 12:57:56 -0500

    Fix an infinite loop in the elaboration of lambdas.

--- src/Tile/Elab.hs
+++ src/Tile/Elab.hs
@@ -43,7 +43,7 @@ instance (Ord v, Show v, Let v t, Lam v t, Prob v t, Type v t, Err t) => Lam v (
   lam p b = check $ \ exp ->
     type' `ex` \ _A ->
     (var _A --> type') `ex` \ _B ->
-    exp === (lam p (\ x -> x ::: var _A |- elab (b x ::: var _B $$ var x)) ::: ((p, var _A) >-> \ x -> var _B $$ var x))
+    exp === (Elab (lam p (\ x -> let Elab m = (x ::: var _A |- elab (b x ::: var _B $$ var x)) in m)) ::: ((p, var _A) >-> \ x -> var _B $$ var x))
 
   f $$ a = check $ \ exp ->
     type' `ex` \ _A ->
