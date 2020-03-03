d5b094d79a1104221c3090dec9778f0a53336b18
Rob Rix <rob.rix@me.com>
2020-03-03 14:09:36 -0500

    Allow separate wrapping of the variable/type.

--- src/Tile/Print.hs
+++ src/Tile/Print.hs
@@ -88,7 +88,7 @@ instance Type Int (Print Inner) where
 
 instance Prob Int (Print Inner) where
   ex t b = inContext Exists . prec (Level 0) . bind b $ \ v b ->
-    pretty '∃' <+> prettyAnn (prettyBind v ::: t) </> group (align (op "." <+> b))
+    pretty '∃' <+> group (align (prettyAnn (prettyBind v ::: t))) </> group (align (op "." <+> b))
 
   t1 === t2 = inContext Equate (prec (Level 4) (prettyAnn t1 <+> op "≡" <+> prettyAnn t2))
 
