5f176434c73278fc0f714c82413dde6ff5adb8bd
Rob Rix <rob.rix@me.com>
2020-03-04 08:12:28 -0500

    :fire: the last parameter to Elab.

--- src/Tile/Elab.hs
+++ src/Tile/Elab.hs
@@ -18,15 +18,15 @@ import Data.Maybe (fromMaybe)
 import Tile.Context
 import Tile.Syntax
 
-elab :: Map v t :|-: Elab v t t ::: t -> t
+elab :: Map v t :|-: Elab v t ::: t -> t
 elab (ctx :|-: Elab m ::: t) = m t ctx
 
-newtype Elab v t a = Elab { runElabC :: t -> Map v t -> a }
+newtype Elab v t = Elab { runElabC :: t -> Map v t -> t }
 
-instance (Ord v, Prob v t, Err v t) => Var v (Elab v t t) where
+instance (Ord v, Prob v t, Err v t) => Var v (Elab v t) where
   var n = check $ \ ctx -> pure (var n ::: typeOf ctx n)
 
-instance (Ord v, Let v t, Prob v t, Type v t, Err v t) => Let v (Elab v t t) where
+instance (Ord v, Let v t, Prob v t, Type v t, Err v t) => Let v (Elab v t) where
   let' (v ::: t) b = check $ \ ctx -> do
     _B <- meta type'
     t' <- letbind ((ctx |- t ::: type')  ::: type')
@@ -35,7 +35,7 @@ instance (Ord v, Let v t, Prob v t, Type v t, Err v t) => Let v (Elab v t t) whe
             ctx |> x ::: var t' |- b x ::: var _B)
       ::: var _B)
 
-instance (Ord v, Let v t, Lam v t, Prob v t, Type v t, Err v t) => Lam v (Elab v t t) where
+instance (Ord v, Let v t, Lam v t, Prob v t, Type v t, Err v t) => Lam v (Elab v t) where
   lam p b = check $ \ ctx -> do
     _A <- meta type'
     _B <- meta (var _A --> type')
@@ -50,7 +50,7 @@ instance (Ord v, Let v t, Lam v t, Prob v t, Type v t, Err v t) => Lam v (Elab v
       (   (ctx |- f ::: var _A --> var _B) $$ (ctx |- a ::: var _A)
       ::: var _B)
 
-instance (Ord v, Let v t, Prob v t, Type v t, Err v t) => Type v (Elab v t t) where
+instance (Ord v, Let v t, Prob v t, Type v t, Err v t) => Type v (Elab v t) where
   type' = check (const (pure (type' ::: type')))
 
   (p, a) >-> b = check $ \ ctx -> do
@@ -59,15 +59,15 @@ instance (Ord v, Let v t, Prob v t, Type v t, Err v t) => Type v (Elab v t t) wh
       (   (p, var a') >-> (\ x -> ctx |> x ::: var a' |- b x ::: type')
       ::: type')
 
-deriving instance (Ord v, Prob v t, Err v t) => Prob v (Elab v t t)
+deriving instance (Ord v, Prob v t, Err v t) => Prob v (Elab v t)
 
-deriving instance Err v t => Err v (Elab v t t)
+deriving instance Err v t => Err v (Elab v t)
 
 
 typeOf :: (Ord v, Err v t) => Map v t -> v -> t
 typeOf ctx n = fromMaybe (freeVariable n) (ctx !? n)
 
-(|-) :: Map v t -> Elab v t t ::: t -> t
+(|-) :: Map v t -> Elab v t ::: t -> t
 ctx |- (b ::: t) = elab (ctx :|-: b ::: t)
 
 infixl 1 |-
@@ -77,7 +77,7 @@ ctx |> (v ::: t) = insert v t ctx
 
 infixl 1 |>
 
-check :: Prob v t => (Map v t -> Script t (t ::: t)) -> Elab v t t
+check :: Prob v t => (Map v t -> Script t (t ::: t)) -> Elab v t
 check f = Elab $ \ ty ctx -> runScript id $ do
   exp <- meta ty
   act <- f ctx
