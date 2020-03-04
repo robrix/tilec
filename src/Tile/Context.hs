a0588cdc8ad672ec44ec7600734c554448728a22
Rob Rix <rob.rix@me.com>
2020-03-03 20:13:23 -0500

    Define an entailment type.

--- src/Tile/Context.hs
+++ src/Tile/Context.hs
@@ -1,2 +1,27 @@
+{-# LANGUAGE DeriveTraversable #-}
+{-# LANGUAGE TypeOperators #-}
 module Tile.Context
-() where
+( (:|-:)(..)
+) where
+
+import Data.Bifoldable
+import Data.Bifunctor
+import Data.Bitraversable
+import Data.Functor.Classes
+
+data a :|-: b = a :|-: b
+  deriving (Eq, Foldable, Functor, Ord, Show, Traversable)
+
+instance Bifoldable (:|-:) where
+  bifoldMap = bifoldMapDefault
+
+instance Bifunctor (:|-:) where
+  bimap = bimapDefault
+
+instance Bitraversable (:|-:) where
+  bitraverse f g (l :|-: r) = (:|-:) <$> f l <*> g r
+
+instance Show a => Show1 ((:|-:) a) where
+  liftShowsPrec sp _ p (a :|-: b) = showParen (p > 1) $ shows a . showString " :|-: " . sp 2 b
+
+infixl 1 :|-:
