466d78cead0fa4264555227bd55a65cf39d1b648
Rob Rix <rob.rix@me.com>
2020-03-05 09:48:46 -0500

    Define s-expression parsers.

--- src/Tile/Parse.hs
+++ src/Tile/Parse.hs
@@ -1,7 +1,21 @@
 module Tile.Parse
 ( SExpr(..)
+, sexpr_
 ) where
+
+import Control.Applicative (Alternative(..))
+import Text.Parser.Char
+import Text.Parser.Token
 
 class SExpr t where
   atom :: String -> t
   list :: [t] -> t
+
+sexpr_ :: (TokenParsing m, SExpr t) => m t
+sexpr_ = list_ <|> atom_
+
+atom_ :: (TokenParsing m, SExpr t) => m t
+atom_ = atom <$> token (many alphaNum)
+
+list_ :: (TokenParsing m, SExpr t) => m t
+list_ = list <$> parens (many sexpr_)
