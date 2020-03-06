c90340136e6e906a400e91bafbfaca70b69a4aaa
Rob Rix <rob.rix@me.com>
2020-03-06 08:06:16 -0500

    Represent atoms as one or more characters.

--- src/Tile/Parse.hs
+++ src/Tile/Parse.hs
@@ -8,6 +8,7 @@ import Control.Applicative (Alternative(..))
 import Control.Carrier.Parser.Church
 import Control.Carrier.Throw.Either
 import Control.Effect.Parser.Notice
+import Data.List.NonEmpty
 import Data.Semilattice.Lower
 import Text.Parser.Char
 import Text.Parser.Token
@@ -17,14 +18,14 @@ parse s = run (runThrow (runParserWithString lowerBound s sexpr_))
 
 
 class SExpr t where
-  atom :: String -> t
+  atom :: NonEmpty Char -> t
   list :: [t] -> t
 
 sexpr_ :: (TokenParsing m, SExpr t) => m t
 sexpr_ = list_ <|> atom_
 
 atom_ :: (TokenParsing m, SExpr t) => m t
-atom_ = atom <$> token (some alphaNum)
+atom_ = atom <$> token (some1 alphaNum)
 
 list_ :: (TokenParsing m, SExpr t) => m t
 list_ = list <$> parens (many sexpr_)
