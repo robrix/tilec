fb2efe03d87fb86e4dd89239745f312f16ad5d7d
Rob Rix <rob.rix@me.com>
2020-03-07 02:24:48 -0500

    Derive a FreeVariable instance for Identity.

--- src/Tile/Error.hs
+++ src/Tile/Error.hs
@@ -1,7 +1,15 @@
+{-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE FunctionalDependencies #-}
+{-# LANGUAGE GeneralizedNewtypeDeriving #-}
+{-# LANGUAGE StandaloneDeriving #-}
+{-# LANGUAGE UndecidableInstances #-}
 module Tile.Error
 ( FreeVariable(..)
 ) where
 
+import Data.Functor.Identity
+
 class FreeVariable v e | e -> v where
   freeVariable :: v -> e
+
+deriving instance FreeVariable v e => FreeVariable v (Identity e)
