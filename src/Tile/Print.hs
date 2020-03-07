0b436e789e57a6e6906e6395a83f1a5cbe75e9f0
Rob Rix <rob.rix@me.com>
2020-03-07 15:47:44 -0500

    Lift PrintC over an underlying monad.

--- src/Tile/Print.hs
+++ src/Tile/Print.hs
@@ -7,6 +7,7 @@
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE UndecidableInstances #-}
 module Tile.Print
 ( prettyPrint
 , prettyPrintWith
@@ -20,10 +21,9 @@ import           Control.Applicative ((<**>))
 import           Control.Carrier.Fresh.Strict
 import           Control.Carrier.State.Strict
 import           Control.Carrier.Writer.Strict
-import           Control.Monad (guard)
+import           Control.Monad (guard, (<=<))
 import           Control.Monad.IO.Class
 import           Data.Function (on)
-import           Data.Functor.Identity
 import qualified Data.IntSet as IntSet
 import           Data.Monoid (Ap(..))
 import qualified Data.Text.Prettyprint.Doc as PP
@@ -33,11 +33,11 @@ import           Tile.Pretty hiding (Doc, PrecDoc)
 import qualified Tile.Pretty as P
 import           Tile.Syntax
 
-prettyPrint :: MonadIO m => PrintC Doc -> m ()
+prettyPrint :: MonadIO m => PrintC m Doc -> m ()
 prettyPrint = prettyPrintWith defaultStyle
 
-prettyPrintWith :: MonadIO m => (Highlight Int -> ANSI.AnsiStyle) -> PrintC Doc -> m ()
-prettyPrintWith style  = putDoc . PP.reAnnotate style . toDoc . runPrint
+prettyPrintWith :: MonadIO m => (Highlight Int -> ANSI.AnsiStyle) -> PrintC m Doc -> m ()
+prettyPrintWith style  = putDoc . PP.reAnnotate style . toDoc <=< runPrint
 
 defaultStyle :: Highlight Int -> ANSI.AnsiStyle
 defaultStyle = \case
@@ -79,49 +79,49 @@ instance Ord V where compare = compare `on` vvar
 instance Show V where showsPrec p = showsPrec p . vdoc
 
 
-runPrint :: PrintC a -> a
-runPrint = run . fmap snd . runWriter . evalFresh 0 . evalState Nothing . getAp . runPrintC
+runPrint :: Functor m => PrintC m a -> m a
+runPrint = fmap snd . runWriter . evalFresh 0 . evalState Nothing . getAp . runPrintC
 
-newtype PrintC a = PrintC { runPrintC :: Ap (StateC (Maybe Ctx) (FreshC (WriterC IntSet.IntSet Identity))) a }
+newtype PrintC m a = PrintC { runPrintC :: Ap (StateC (Maybe Ctx) (FreshC (WriterC IntSet.IntSet m))) a }
   deriving (Applicative, Functor, Monad, Monoid, Semigroup)
 
-deriving instance P.Doc     (Highlight Int) (PrintC Doc)
-deriving instance P.PrecDoc (Highlight Int) (PrintC Doc)
+deriving instance Monad m => P.Doc     (Highlight Int) (PrintC m Doc)
+deriving instance Monad m => P.PrecDoc (Highlight Int) (PrintC m Doc)
 
-instance Show (PrintC Doc) where
+instance (Functor m, Show (m Doc)) => Show (PrintC m Doc) where
   showsPrec p = showsPrec p . runPrint
 
-instance Var V Doc PrintC where
+instance Algebra sig m => Var V Doc (PrintC m) where
   var v = inContext Var (PrintC (vdoc v <$ tell (IntSet.singleton (vvar v))))
 
-instance Let V Doc PrintC where
+instance Algebra sig m => Let V Doc (PrintC m) where
   let' (tm ::: ty) b = inContext Let . bind b $ \ v b ->
     -- FIXME: bind variables on the lhs when tm is a lambda
     kw "let" <+> prettyBind v <+> group (align (prettyAnn (op "=" <+> tm ::: ty))) <+> kw "in" </> b
 
-instance Lam V Doc PrintC where
+instance Algebra sig m => Lam V Doc (PrintC m) where
   lam p b = prec (Level 6) . inContext Lam . bind b $ \ v b ->
     plicit braces id p (prettyBind v) <+> b
 
   f $$ a = prec (Level 10) (inContext App (f </> prec (Level 11) a))
 
-instance Type V Doc PrintC where
+instance Algebra sig m => Type V Doc (PrintC m) where
   type' = inContext Type (annotate TypeName (pretty "Type"))
 
   (p, t) >-> b = prec (Level 6) . inContext Pi . bind b $ \ v b ->
     group (align (maybe (plicit braces (prec (Level 7)) p t) (group . align . plicit braces parens p . prettyAnn . (::: t) . pure . vdoc) v </> op "→" <+> b))
 
-instance Prob V Doc PrintC where
+instance Algebra sig m => Prob V Doc (PrintC m) where
   ex t b = prec (Level 6) . inContext Exists . bind (b . toMeta) $ \ v b ->
     group (align (op "∃" <+> group (align (reset (Level 0) (prettyAnn (prettyBind (toMeta <$> v) ::: t)))) <+> op "." </> reset (Level 0) b)) where
     toMeta v = v { vdoc = annotate MetaVar (pretty '?' <> vdoc v) }
 
   t1 === t2 = prec (Level 4) (inContext Equate (group (align (flatAlt (space <> space) mempty <> prec (Level 5) (prettyAnn t1) </> op "≡" <+> prec (Level 5) (prettyAnn t2)))))
 
-instance Err (PrintC Doc) Doc PrintC where
+instance Algebra sig m => Err (PrintC m Doc) Doc (PrintC m) where
   err = id
 
-instance FreeVariable V (PrintC Doc) where
+instance Algebra sig m => FreeVariable V (PrintC m Doc) where
   freeVariable v = annotate Error (pretty "error") <> pretty ':' <+> pure (vdoc v)
 
 data Highlight a
@@ -157,7 +157,7 @@ data Ctx
   | Equate
   deriving (Eq, Ord, Show)
 
-transition :: Maybe Ctx -> Maybe Ctx -> PrintC Doc -> PrintC Doc
+transition :: Monad m => Maybe Ctx -> Maybe Ctx -> PrintC m Doc -> PrintC m Doc
 transition from to = exit from . enter to where
   enter = \case
     Just Let    -> group . align
@@ -171,7 +171,7 @@ transition from to = exit from . enter to where
     Just Lam -> (op "." </>) . reset (Level 0)
     _ -> id
 
-inContext :: Ctx -> PrintC Doc -> PrintC Doc
+inContext :: Algebra sig m => Ctx -> PrintC m Doc -> PrintC m Doc
 inContext ctx m = do
   ctx' <- PrintC get
   if ctx' == Just ctx then
@@ -188,7 +188,7 @@ kw = annotate Keyword . pretty
 op :: P.Doc (Highlight Int) doc => String -> doc
 op = annotate Op . pretty
 
-prettyBind :: Maybe V -> PrintC Doc
+prettyBind :: Monad m => Maybe V -> PrintC m Doc
 prettyBind = maybe (pretty '_') (pure . vdoc)
 
 prettyVar :: P.Doc (Highlight Int) doc => Int -> doc
@@ -199,7 +199,7 @@ prettyVar i = annotate Name (pretty (alphabet !! r) <> if q > 0 then pretty q el
 prettyAnn :: P.PrecDoc (Highlight Int) doc => doc ::: doc -> doc
 prettyAnn (tm ::: ty) = group (prec (Level 6) tm </> group (align (op ":" <+> prec (Level 6) ty)))
 
-bind :: (V -> PrintC a) -> (Maybe V -> PrintC a -> PrintC b) -> PrintC b
+bind :: Algebra sig m => (V -> PrintC m a) -> (Maybe V -> PrintC m a -> PrintC m b) -> PrintC m b
 bind b f = PrintC $ do
   v <- fresh
   let v' = V v (prettyVar v)
