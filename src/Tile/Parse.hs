b854e09c89e871c7e084c99975d4ff22259ca813
Rob Rix <rob.rix@me.com>
2020-03-06 22:46:22 -0500

    Parse free & bound variables.

--- src/Tile/Parse.hs
+++ src/Tile/Parse.hs
@@ -1,28 +1,37 @@
+{-# LANGUAGE ScopedTypeVariables #-}
+{-# LANGUAGE TypeApplications #-}
 module Tile.Parse
 ( parse
 , expr_
 ) where
 
-import Control.Applicative (Alternative(..))
-import Control.Carrier.Parser.Church
-import Control.Carrier.Throw.Either
-import Control.Effect.Parser.Notice
-import Data.HashSet (HashSet, fromList)
-import Data.Semilattice.Lower
-import Text.Parser.Char
-import Text.Parser.Token
-import Text.Parser.Token.Highlight
-import Tile.Syntax
+import           Control.Applicative (Alternative(..))
+import           Control.Carrier.Parser.Church
+import           Control.Carrier.Reader
+import           Control.Carrier.Throw.Either
+import           Control.Effect.Parser.Notice
+import           Data.HashSet (HashSet, fromList)
+import qualified Data.Map as Map
+import           Data.Semilattice.Lower
+import           Text.Parser.Char
+import           Text.Parser.Token
+import           Text.Parser.Token.Highlight
+import           Tile.Syntax
 
-parse :: Type v t => String -> Either Notice t
-parse s = run (runThrow (runParserWithString lowerBound s expr_))
+parse :: forall v t . (Free v t, Type v t) => String -> Either Notice t
+parse s = run (runThrow (runReader (mempty @(Map.Map String v)) (runParserWithString lowerBound s expr_)))
 
 
-expr_ :: (Monad m, TokenParsing m, Type v t) => m t
-expr_ = type_
+expr_ :: (Has (Reader (Map.Map String v)) sig m, TokenParsing m, Free v t, Type v t) => m t
+expr_ = type_ <|> var_
 
 identifier_ :: (Monad m, TokenParsing m) => m String
 identifier_ = ident identifierStyle
+
+var_ :: (Has (Reader (Map.Map String v)) sig m, TokenParsing m, Free v t) => m t
+var_ = do
+  v <- identifier_
+  asks (maybe (free v) var . Map.lookup v)
 
 type_ :: (Monad m, TokenParsing m, Type v t) => m t
 type_ = type' <$ reserve identifierStyle "Type"
