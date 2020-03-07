9ddba7000915951ae5c0ccc0db4e5325634ab69e
Rob Rix <rob.rix@me.com>
2020-03-06 22:25:26 -0500

    :fire: SExpr & Surface.

--- src/Tile/Parse.hs
+++ src/Tile/Parse.hs
@@ -1,10 +1,5 @@
-{-# LANGUAGE LambdaCase #-}
-{-# LANGUAGE UndecidableInstances #-}
 module Tile.Parse
 ( parse
-, SExpr(..)
-, sexpr_
-, Surface(..)
 ) where
 
 import Control.Applicative (Alternative(..))
@@ -16,33 +11,17 @@ import Data.Semilattice.Lower
 import Text.Parser.Char
 import Text.Parser.Token
 import Text.Parser.Token.Highlight
-import Tile.Syntax as Syn
+import Tile.Syntax
 
-parse :: SExpr t => String -> Either Notice t
-parse s = run (runThrow (runParserWithString lowerBound s sexpr_))
+parse :: Type v t => String -> Either Notice t
+parse s = run (runThrow (runParserWithString lowerBound s type_))
 
-
-class SExpr t where
-  identifier :: String -> t
-
-  type' :: t
-
-  list :: [t] -> t
-
-sexpr_ :: (Monad m, TokenParsing m, SExpr t) => m t
-sexpr_ = list_ <|> atom_
-
-atom_ :: (Monad m, TokenParsing m, SExpr t) => m t
-atom_ = identifier <$> identifier_ <|> type_
 
 identifier_ :: (Monad m, TokenParsing m) => m String
 identifier_ = ident identifierStyle
 
-type_ :: (Monad m, TokenParsing m, SExpr t) => m t
-type_ = Tile.Parse.type' <$ reserve identifierStyle "Type"
-
-list_ :: (Monad m, TokenParsing m, SExpr t) => m t
-list_ = list <$> parens (many sexpr_)
+type_ :: (Monad m, TokenParsing m, Type v t) => m t
+type_ = type' <$ reserve identifierStyle "Type"
 
 identifierStyle :: CharParsing m => IdentifierStyle m
 identifierStyle = IdentifierStyle "identifier" letter (alphaNum <|> char '\'') reservedWords Identifier ReservedIdentifier
@@ -54,23 +33,3 @@ reservedWords = fromList
   , "module"
   , "import"
   ]
-
-
-newtype Surface t = Surface { runSurface :: [t] -> Either String t }
-
-instance Type v t => SExpr (Surface t) where
-  identifier = \case
-    "Type" -> Surface $ \case
-      [] -> Right Syn.type'
-      _  -> Left "unexpected arguments to Type"
-    "->"   -> Surface $ \case
-      []  -> Left "0 arguments given to ->"
-      [_] -> Left "1 argument given to ->"
-      ts  -> Right $ foldr1 (-->) ts
-    other  -> Surface $ \ _ -> Left $ "unknown atom: " <> show other
-
-  type' = Surface $ \case
-    [] -> Right Syn.type'
-    _  -> Left "unexpected arguments to Type"
-
-  list _ = Surface (const (Left "unimplemented: list"))
