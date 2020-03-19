236778c2140d195cc371271a72d0000e6b1493f5
Rob Rix <rob.rix@me.com>
2020-03-19 11:48:51 -0400

    Define a lifting of appPure into Applicative contexts.

--- src/Tile/Permutable.hs
+++ src/Tile/Permutable.hs
@@ -1,7 +1,15 @@
 module Tile.Permutable
 ( Lam(..)
+, ($$)
 ) where
+
+import Control.Applicative (liftA2)
 
 class Lam repr where
   lamPure :: (repr a -> repr b) -> repr (a -> b)
   appPure :: repr (a -> b) -> (repr a -> repr b)
+
+($$) :: (Lam repr, Applicative m) => m (repr (a -> b)) -> (m (repr a) -> m (repr b))
+($$) = liftA2 appPure
+
+infixl 9 $$
