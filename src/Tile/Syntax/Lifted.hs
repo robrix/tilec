97d3ff717446ff9663048aadf192534e5ffcd6db
Rob Rix <rob.rix@me.com>
2020-03-20 13:15:57 -0400

    Move let'' down.

--- src/Tile/Syntax/Lifted.hs
+++ src/Tile/Syntax/Lifted.hs
@@ -58,19 +58,19 @@ var = C . pure . fmap S.var
 
 -- Let
 
-let''
-  :: (Applicative m, S.Let v expr, Permutable i)
-  => (m :.: i) expr ::: (m :.: i) expr
-  -> (forall j . Permutable j => j v -> (m :.: i :.: j) expr)
-  -> (m :.: i) expr
-let'' (tm ::: ty) f = S.let' <$> ((:::) <$> tm <*> ty) <*> (C . fmap getC . getC) (f id)
-
 let'
   :: (Applicative m, S.Let v expr, Permutable i)
   => (m :.: i) expr ::: (m :.: i) expr
   -> (forall j . Permutable j => (i :.: j) v -> (m :.: i :.: j) expr)
   -> (m :.: i) expr
 let' v f = let'' v (f . C . pure)
+
+let''
+  :: (Applicative m, S.Let v expr, Permutable i)
+  => (m :.: i) expr ::: (m :.: i) expr
+  -> (forall j . Permutable j => j v -> (m :.: i :.: j) expr)
+  -> (m :.: i) expr
+let'' (tm ::: ty) f = S.let' <$> ((:::) <$> tm <*> ty) <*> (C . fmap getC . getC) (f id)
 
 
 -- Lam
