c4f0f8aae4ab587d4a19fad2e61ec691360a65fd
Rob Rix <rob.rix@me.com>
2020-03-20 12:52:09 -0400

    Write Script.<*> pointfully.

--- src/Tile/Syntax/Lifted.hs
+++ src/Tile/Syntax/Lifted.hs
@@ -148,5 +148,5 @@ instance Applicative m => Applicative (Script t m) where
   pure a = Script $ \ k -> strengthen (k (pure a))
   {-# INLINE pure #-}
 
-  Script f <*> Script a = Script $ \ k -> f (\ f' -> assocL (a (assocLR . k . (assocR (liftC f') <*>) . assocRL)))
+  Script f <*> Script a = Script $ \ k -> f (\ f' -> assocL (a (\ a' -> assocLR (k (assocR (liftC f') <*> assocRL a')))))
   {-# INLINE (<*>) #-}
