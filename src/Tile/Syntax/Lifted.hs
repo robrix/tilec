d09fddfe5b5f4980c9acbe4196aba46612f91334
Rob Rix <rob.rix@me.com>
2020-03-20 10:25:02 -0400

    Add headings.

--- src/Tile/Syntax/Lifted.hs
+++ src/Tile/Syntax/Lifted.hs
@@ -4,15 +4,21 @@
 module Tile.Syntax.Lifted
 ( Permutable
 , S.Syntax
+  -- * Var
 , S.Var
 , var
+  -- * Let
 , S.Let
 , let'
+  -- * Lam
 , S.Lam
 , lam
+  -- * Type
 , S.Type
+  -- * Prob
 , S.Prob
 , ex
+  -- * Modules
 , S.Def
   -- * Re-exports
 , (:::)(..)
@@ -28,14 +34,25 @@ import           Tile.Type
 
 type Permutable f = (Applicative f, Distributive f)
 
+-- Var
+
 var :: (Applicative m, Functor i, S.Var v expr) => i v -> m (i expr)
 var = pure . fmap S.var
+
+
+-- Let
 
 let' :: (Applicative m, S.Let v expr, Permutable i) => (m :.: i) expr ::: (m :.: i) expr -> (forall j . Permutable j => (i :.: j) v -> (m :.: i :.: j) expr) -> (m :.: i) expr
 let' (tm ::: ty) f = S.let' <$> ((:::) <$> tm <*> ty) <*> mapC (fmap getC) (f (C (pure id)))
 
+
+-- Lam
+
 lam :: (Applicative m, S.Lam v expr, Permutable i) => (m :.: i) Plicit -> (forall j . Permutable j => (i :.: j) v -> (m :.: i :.: j) expr) -> (m :.: i) expr
 lam p f = S.lam <$> p <*> mapC (fmap getC) (f (C (pure id)))
+
+
+-- Prob
 
 ex :: (Applicative m, S.Prob v expr, Permutable i) => (m :.: i) expr -> (forall j . Permutable j => (i :.: j) v -> (m :.: i :.: j) expr) -> (m :.: i) expr
 ex t f = S.ex <$> t <*> mapC (fmap getC) (f (C (pure id)))
