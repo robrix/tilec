af5afe6c2a6cd96f598ccf488f659440b8fdb0b5
Rob Rix <rob.rix@me.com>
2020-03-20 14:50:34 -0400

    :fire: the derived syntax instances for Tile.Reconstruct.

--- src/Tile/Reconstruct.hs
+++ src/Tile/Reconstruct.hs
@@ -6,7 +6,5 @@ module Tile.Reconstruct
 ( Reconstruct(..)
 ) where
 
-import Tile.Syntax
-
 newtype Reconstruct a m b = Reconstruct { runReconstruct :: m b }
-  deriving (Applicative, Functor, Monad, Var v, Lam v, Let v, Type v, Prob v)
+  deriving (Applicative, Functor, Monad)
