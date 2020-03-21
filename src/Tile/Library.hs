88edd793d5b618055505910a41ede625f816560c
Rob Rix <rob.rix@me.com>
2020-03-21 11:03:36 -0400

    Placate hlint.

--- src/Tile/Library.hs
+++ src/Tile/Library.hs
@@ -53,7 +53,7 @@ nothing :: (Lam expr, Type expr) => expr ::: expr
 nothing = lam Ex (lam Ex . const) ::: (Im, type') >-> \ _A -> tm maybe $$ _A
 
 just :: (Lam expr, Type expr) => expr ::: expr
-just = lam Ex (\ a -> lam Ex (const (lam Ex (\ just -> just $$ a)))) ::: (Im, type') >-> \ _A -> _A --> tm maybe $$ _A
+just = lam Ex (\ a -> lam Ex (const (lam Ex ($$ a)))) ::: (Im, type') >-> \ _A -> _A --> tm maybe $$ _A
 
 
 either :: (Lam expr, Type expr) => expr ::: expr
@@ -63,7 +63,7 @@ left :: (Lam expr, Type expr) => expr ::: expr
 left = lam Ex (\ l -> lam Ex (\ left -> lam Ex (const (left $$ l)))) ::: (Im, type') >-> \ _L -> (Im, type') >-> \ _R -> _L --> tm either $$ _L $$ _R
 
 right :: (Lam expr, Type expr) => expr ::: expr
-right = lam Ex (\ r -> lam Ex (const (lam Ex (\ right -> right $$ r)))) ::: (Im, type') >-> \ _L -> (Im, type') >-> \ _R -> _R --> tm either $$ _L $$ _R
+right = lam Ex (\ r -> lam Ex (const (lam Ex ($$ r)))) ::: (Im, type') >-> \ _L -> (Im, type') >-> \ _R -> _R --> tm either $$ _L $$ _R
 
 
 nat :: Type expr => expr ::: expr
@@ -73,7 +73,7 @@ z :: (Lam expr, Type expr) => expr ::: expr
 z = lam Ex (lam Ex . const) ::: tm nat
 
 s :: (Lam expr, Type expr) => expr ::: expr
-s = lam Ex (\ x -> lam Ex (const (lam Ex (\ s -> s $$ x)))) ::: tm nat --> tm nat
+s = lam Ex (\ x -> lam Ex (const (lam Ex ($$ x)))) ::: tm nat --> tm nat
 
 
 list :: (Lam expr, Type expr) => expr ::: expr
