b2285487a96c7d15898df218d0bb7e1f107d758e
Rob Rix <rob.rix@me.com>
2020-03-21 12:01:21 -0400

    Add headings.

--- src/Tile/Library.hs
+++ src/Tile/Library.hs
@@ -30,6 +30,8 @@ import Prelude hiding (either, maybe)
 import Tile.Syntax
 import Tile.Type
 
+-- Booleans
+
 bool :: Type expr => expr ::: expr
 bool = ((Im, type') >-> \ _A -> _A --> _A --> _A) ::: type'
 
@@ -39,6 +41,8 @@ true = lam Ex (lam Ex . const) ::: tm bool
 false :: (Lam expr, Type expr) => expr ::: expr
 false = lam Ex (const (lam Ex id)) ::: tm bool
 
+
+-- Functions
 
 id' :: (Lam expr, Type expr) => expr ::: expr
 id' = lam Ex id ::: (Im, type') >-> \ _A -> _A --> _A
@@ -50,6 +54,8 @@ fix :: (Lam expr, Let expr, Type expr) => expr ::: expr
 fix = lam Im (\ _A -> lam Im (\ _B -> lam Ex (\ f -> let' (tm fix $$ f ::: _A --> _B) (\ fixf -> lam Ex (\ a -> f $$ fixf $$ a))))) ::: type' ==> \ _A -> type' ==> \ _B -> ((_A --> _B) --> (_A --> _B)) --> (_A --> _B)
 
 
+-- Maybe
+
 maybe :: (Lam expr, Type expr) => expr ::: expr
 maybe = lam Ex (\ _A -> (Im, type') >-> \ _R -> _R --> (_A --> _R) --> _R) ::: type' --> type'
 
@@ -59,6 +65,8 @@ nothing = lam Ex (lam Ex . const) ::: (Im, type') >-> \ _A -> tm maybe $$ _A
 just :: (Lam expr, Type expr) => expr ::: expr
 just = lam Ex (\ a -> lam Ex (const (lam Ex ($$ a)))) ::: (Im, type') >-> \ _A -> _A --> tm maybe $$ _A
 
+
+-- Either
 
 either :: (Lam expr, Type expr) => expr ::: expr
 either = lam Ex (\ _L -> lam Ex (\ _R -> (Im, type') >-> \ _K -> (_L --> _K) --> (_R --> _K) --> _K)) ::: type' --> type' --> type'
@@ -70,6 +78,8 @@ right :: (Lam expr, Type expr) => expr ::: expr
 right = lam Ex (\ r -> lam Ex (const (lam Ex ($$ r)))) ::: (Im, type') >-> \ _L -> (Im, type') >-> \ _R -> _R --> tm either $$ _L $$ _R
 
 
+-- Nat
+
 nat :: Type expr => expr ::: expr
 nat = ((Im, type') >-> \ _R -> _R --> (_R --> _R) --> _R) ::: type'
 
@@ -79,6 +89,8 @@ z = lam Ex (lam Ex . const) ::: tm nat
 s :: (Lam expr, Type expr) => expr ::: expr
 s = lam Ex (\ x -> lam Ex (const (lam Ex ($$ x)))) ::: tm nat --> tm nat
 
+
+-- List
 
 list :: (Lam expr, Type expr) => expr ::: expr
 list = lam Ex (\ _A -> (Im, type') >-> \ _R -> _R --> (_A --> _R --> _R) --> _R) ::: type' --> type'
