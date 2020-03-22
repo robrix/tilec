5b855b74d49c1bc707562564366a14b85147842c
Rob Rix <rob.rix@me.com>
2020-03-22 15:22:30 -0400

    Add an example of inserting an implicit lambda.

--- src/Tile/Reconstruct.hs
+++ src/Tile/Reconstruct.hs
@@ -1,4 +1,17 @@
 -- | Type reconstruction in tagless final style.
+--
+-- Inserting implicit lambdas:
+--
+-- @
+-- id
+--   : { a : Type } -> a -> a
+--   = \ a . a
+-- @
+-- @
+-- id
+--   : { a : Type } -> a -> a
+--   = \ {_} a . a
+-- @
 module Tile.Reconstruct
 ( Reconstruct(..)
 ) where
