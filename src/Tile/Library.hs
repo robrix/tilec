808144f737e34e3accb375978b5f2a441069c888
Rob Rix <rob.rix@me.com>
2020-03-26 17:25:09 -0400

    Simplify the library definitions using lams.

--- src/Tile/Library.hs
+++ src/Tile/Library.hs
@@ -55,11 +55,11 @@ baseBool = module' "Base.Bool" . runScript export $ do
 
   false <- "False"
     .: bool
-    := lam Ex (const (lam Ex id))
+    := lams (\ _ b -> I b)
 
   true <- "True"
     .: bool
-    := lam Ex (lam Ex . const)
+    := lams (\ a _ -> I a)
 
   pure (record
     [ bool
@@ -71,10 +71,10 @@ bool :: Type expr => expr ::: expr
 bool = (type' ==> \ _A -> _A --> _A --> _A) ::: type'
 
 false :: (Lam expr, Type expr) => expr ::: expr
-false = lam Ex (const (lam Ex id)) ::: tm bool
+false = lams (\ _ b -> I b) ::: tm bool
 
 true :: (Lam expr, Type expr) => expr ::: expr
-true = lam Ex (lam Ex . const) ::: tm bool
+true = lams (\ a _ -> I a) ::: tm bool
 
 
 -- Functions
@@ -83,11 +83,11 @@ baseFunction :: (Module decl m, Def expr decl, Export decl, Lam expr, Let expr, 
 baseFunction = module' "Base.Function" . runScript export $ do
   id <- "id"
     .: type' ==> (\ _A -> _A --> _A)
-    := lam Ex id
+    := lams I
 
   const <- "const"
     .: type' ==> (\ _A -> type' ==> \ _B -> _A --> _B --> _A)
-    := lam Ex (lam Ex . const)
+    := lams (\ a _ -> I a)
 
   fix <- "fix"
     .: type' ==> (\ _A -> type' ==> \ _B -> ((_A --> _B) --> (_A --> _B)) --> (_A --> _B))
@@ -100,10 +100,10 @@ baseFunction = module' "Base.Function" . runScript export $ do
     ])
 
 id' :: (Lam expr, Type expr) => expr ::: expr
-id' = lam Ex id ::: type' ==> \ _A -> _A --> _A
+id' = lams I ::: type' ==> \ _A -> _A --> _A
 
 const' :: (Lam expr, Type expr) => expr ::: expr
-const' = lam Ex (lam Ex . const) ::: type' ==> \ _A -> type' ==> \ _B -> _A --> _B --> _A
+const' = lams (\ a _ -> I a) ::: type' ==> \ _A -> type' ==> \ _B -> _A --> _B --> _A
 
 fix :: (Lam expr, Let expr, Type expr) => expr ::: expr
 fix = lam Im (\ _A -> lam Im (\ _B -> lam Ex (\ f -> let' (tm fix $$ f ::: _A --> _B) (\ fixf -> lam Ex (\ a -> f $$ fixf $$ a))))) ::: type' ==> \ _A -> type' ==> \ _B -> ((_A --> _B) --> (_A --> _B)) --> (_A --> _B)
@@ -115,37 +115,37 @@ maybe :: (Lam expr, Type expr) => expr ::: expr
 maybe = lam Ex (\ _A -> type' ==> \ _R -> _R --> (_A --> _R) --> _R) ::: type' --> type'
 
 nothing :: (Lam expr, Type expr) => expr ::: expr
-nothing = lam Ex (lam Ex . const) ::: type' ==> \ _A -> tm maybe $$ _A
+nothing = lams (\ a _ -> I a) ::: type' ==> \ _A -> tm maybe $$ _A
 
 just :: (Lam expr, Type expr) => expr ::: expr
-just = lam Ex (\ a -> lam Ex (const (lam Ex ($$ a)))) ::: type' ==> \ _A -> _A --> tm maybe $$ _A
+just = lams (\ a _ just -> I (just $$ a)) ::: type' ==> \ _A -> _A --> tm maybe $$ _A
 
 
 -- Either
 
 either :: (Lam expr, Type expr) => expr ::: expr
-either = lam Ex (\ _L -> lam Ex (\ _R -> type' ==> \ _K -> (_L --> _K) --> (_R --> _K) --> _K)) ::: type' --> type' --> type'
+either = lams (\ _L _R -> I $ type' ==> \ _K -> (_L --> _K) --> (_R --> _K) --> _K) ::: type' --> type' --> type'
 
 left :: (Lam expr, Type expr) => expr ::: expr
-left = lam Ex (\ l -> lam Ex (\ left -> lam Ex (const (left $$ l)))) ::: type' ==> \ _L -> type' ==> \ _R -> _L --> tm either $$ _L $$ _R
+left = lams (\ l left _ -> I $ left $$ l) ::: type' ==> \ _L -> type' ==> \ _R -> _L --> tm either $$ _L $$ _R
 
 right :: (Lam expr, Type expr) => expr ::: expr
-right = lam Ex (\ r -> lam Ex (const (lam Ex ($$ r)))) ::: type' ==> \ _L -> type' ==> \ _R -> _R --> tm either $$ _L $$ _R
+right = lams (\ r _ right -> I $ right $$ r) ::: type' ==> \ _L -> type' ==> \ _R -> _R --> tm either $$ _L $$ _R
 
 
 -- Pair
 
 pair :: (Lam expr, Type expr) => expr ::: expr
-pair = lam Ex (\ _L -> lam Ex (\ _R -> type' ==> \ _K -> (_L --> _R --> _K) --> _K)) ::: type' --> type' --> type'
+pair = lams (\ _L _R -> I $ type' ==> \ _K -> (_L --> _R --> _K) --> _K) ::: type' --> type' --> type'
 
 pair' :: (Lam expr, Type expr) => expr ::: expr
-pair' = lam Ex (\ l -> lam Ex (\ r -> lam Ex (\ k -> k $$ l $$ r))) ::: type' ==> \ _L -> type' ==> \ _R -> _L --> _R --> tm pair $$ _L $$ _R
+pair' = lams (\ fst snd k -> I $ k $$ fst $$ snd) ::: type' ==> \ _L -> type' ==> \ _R -> _L --> _R --> tm pair $$ _L $$ _R
 
 fst :: (Lam expr, Type expr) => expr ::: expr
-fst = lam Ex ($$ lam Ex (lam Ex . const)) ::: type' ==> \ _L -> type' ==> \ _R -> tm pair $$ _L $$ _R --> _L
+fst = lam Ex ($$ lams (\ fst _ -> I fst)) ::: type' ==> \ _L -> type' ==> \ _R -> tm pair $$ _L $$ _R --> _L
 
 snd :: (Lam expr, Type expr) => expr ::: expr
-snd = lam Ex ($$ lam Ex (const (lam Ex id))) ::: type' ==> \ _L -> type' ==> \ _R -> tm pair $$ _L $$ _R --> _R
+snd = lam Ex ($$ lams (\ _ snd -> I snd)) ::: type' ==> \ _L -> type' ==> \ _R -> tm pair $$ _L $$ _R --> _R
 
 
 -- Nat
@@ -154,10 +154,10 @@ nat :: Type expr => expr ::: expr
 nat = (type' ==> \ _R -> _R --> (_R --> _R) --> _R) ::: type'
 
 z :: (Lam expr, Type expr) => expr ::: expr
-z = lam Ex (lam Ex . const) ::: tm nat
+z = lams (\ z _ -> I z) ::: tm nat
 
 s :: (Lam expr, Type expr) => expr ::: expr
-s = lam Ex (\ x -> lam Ex (const (lam Ex ($$ x)))) ::: tm nat --> tm nat
+s = lams (\ x _ s -> I $ s $$ x) ::: tm nat --> tm nat
 
 
 -- List
@@ -166,10 +166,10 @@ list :: (Lam expr, Type expr) => expr ::: expr
 list = lam Ex (\ _A -> type' ==> \ _R -> _R --> (_A --> _R --> _R) --> _R) ::: type' --> type'
 
 nil :: (Lam expr, Type expr) => expr ::: expr
-nil = lam Ex (lam Ex . const) ::: type' ==> \ _A -> tm list $$ _A
+nil = lams (\ nil _ -> I nil) ::: type' ==> \ _A -> tm list $$ _A
 
 cons :: (Lam expr, Type expr) => expr ::: expr
-cons = lam Ex (\ a -> lam Ex (\ as -> lam Ex (const (lam Ex (\ cons -> cons $$ a $$ as))))) ::: type' ==> \ _A -> tm list $$ _A
+cons = lams (\ a as _ cons -> I $ cons $$ a $$ as) ::: type' ==> \ _A -> tm list $$ _A
 
 
 -- Fin
@@ -178,10 +178,10 @@ fin :: (Lam expr, Type expr) => expr ::: expr
 fin = lam Ex (\ n -> (tm nat --> type') ==> \ _R -> _R $$ (tm s $$ n) --> (tm nat ==> \ n -> _R $$ n --> _R $$ (tm s $$ n)) --> _R $$ (tm s $$ n)) ::: tm nat --> type'
 
 fz :: (Lam expr, Type expr) => expr ::: expr
-fz = lam Ex (lam Ex . const) ::: tm nat ==> \ n -> tm fin $$ (tm s $$ n)
+fz = lams (\ fz _ -> I fz) ::: tm nat ==> \ n -> tm fin $$ (tm s $$ n)
 
 fs :: (Lam expr, Type expr) => expr ::: expr
-fs = lam Ex (\ n -> lam Ex (const (lam Ex ($$ n)))) ::: tm nat ==> \ n -> tm fin $$ n --> tm fin $$ (tm s $$ n)
+fs = lams (\ n _ fs -> I $ fs $$ n) ::: tm nat ==> \ n -> tm fin $$ n --> tm fin $$ (tm s $$ n)
 
 
 -- TODO: vectors
